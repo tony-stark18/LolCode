e09e67048d44ea01078e521301a416a247a180f0
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2024-02-05 16:34:49 +0530

    Time: 11 ms (21.32%) | Memory: 17.3 MB (5.43%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -9,30 +9,41 @@ public:
         q.push({sr,sc});
         ff[sr][sc] = color;
         vis[sr][sc]=1;
+        int dr[] = {-1,1,0,0};
+        int dc[] = {0,0,-1,1};
         while(!q.empty()){
             int r = q.front().first;
             int c = q.front().second;
             q.pop();
-            if(r-1>=0 && image[r][c]==image[r-1][c] && !vis[r-1][c]){
-                ff[r-1][c]=color;
-                q.push({r-1,c});
-                vis[r-1][c]=1;
+            for(int i=0;i<4;i++){
+                int nr = r+dr[i];
+                int nc = c+dc[i];
+                if(nr>=0 && nr<m && nc>=0 && nc<n && image[r][c]==image[nr][nc] && !vis[nr][nc]){
+                    ff[nr][nc]=color;
+                    q.push({nr,nc});
+                    vis[nr][nc]=1;
+                }
             }
-            if(r+1<m && image[r][c]==image[r+1][c] && !vis[r+1][c]){
-                ff[r+1][c]=color;
-                q.push({r+1,c});
-                vis[r+1][c]=1;
-            }
-            if(c-1>=0 && image[r][c]==image[r][c-1] && !vis[r][c-1]){
-                ff[r][c-1]=color;
-                vis[r][c-1]=1;
-                q.push({r,c-1});
-            }
-            if(c+1<n && image[r][c]==image[r][c+1] && !vis[r][c+1]){
-                ff[r][c+1]=color;
-                vis[r][c+1]=1;
-                q.push({r,c+1});
-            }
+            // if(r-1>=0 && image[r][c]==image[r-1][c] && !vis[r-1][c]){
+            //     ff[r-1][c]=color;
+            //     q.push({r-1,c});
+            //     vis[r-1][c]=1;
+            // }
+            // if(r+1<m && image[r][c]==image[r+1][c] && !vis[r+1][c]){
+            //     ff[r+1][c]=color;
+            //     q.push({r+1,c});
+            //     vis[r+1][c]=1;
+            // }
+            // if(c-1>=0 && image[r][c]==image[r][c-1] && !vis[r][c-1]){
+            //     ff[r][c-1]=color;
+            //     vis[r][c-1]=1;
+            //     q.push({r,c-1});
+            // }
+            // if(c+1<n && image[r][c]==image[r][c+1] && !vis[r][c+1]){
+            //     ff[r][c+1]=color;
+            //     vis[r][c+1]=1;
+            //     q.push({r,c+1});
+            // }
 
         }
         return ff;
