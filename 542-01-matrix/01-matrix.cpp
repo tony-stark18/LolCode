3ad88326d026fa6e0c5e2a16686edc7bb618697e
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2024-02-06 17:48:14 +0530

    Time: 51 ms (88.42%) | Memory: 38 MB (17.80%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -3,36 +3,38 @@ public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int n = mat.size();
         int m = mat[0].size();
-        queue<pair<int, int>> q;
-        int dr[] = {-1, 1, 0, 0};
-        int dc[] = {0, 0, -1, 1};
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (mat[i][j] == 0) {
-                    q.push({i, j});
-                } else {
-                    mat[i][j] = INT_MAX;
+        vector<vector<int>> ans(n,vector<int>(m,0));
+        vector<vector<int>> vis(n,vector<int>(m,0));
+        queue<pair<pair<int,int>,int>> q;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j]==0){
+                    q.push({{i,j},0});
+                    vis[i][j]=1;
+                }
+                else{
+                    vis[i][j]=0;
                 }
             }
         }
-
-        while (!q.empty()) {
-            int r = q.front().first;
-            int c = q.front().second;
+        int dr[] = {-1,1,0,0};
+        int dc[] = {0,0,-1,1};
+        while(!q.empty()){
+            int r = q.front().first.first;
+            int c = q.front().first.second;
+            int dis = q.front().second;
             q.pop();
-            
-            for (int i = 0; i < 4; i++) {
-                int nr = r + dr[i];
-                int nc = c + dc[i];
-                
-                if (nr >= 0 && nr < n && nc >= 0 && nc < m && mat[nr][nc] > mat[r][c] + 1) {
-                    mat[nr][nc] = mat[r][c] + 1;
-                    q.push({nr, nc});
+            ans[r][c]=dis;
+            for(int i=0;i<4;i++){
+                int nr = r+dr[i];
+                int nc = c+dc[i];
+                if(nr>=0 && nr<n && nc>=0 && nc<m && !vis[nr][nc]){
+                    q.push({{nr,nc},dis+1});
+                    vis[nr][nc]=1;
                 }
             }
+            
         }
-
-        return mat;
+        return ans;
     }
 };
