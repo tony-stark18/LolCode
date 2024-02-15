c14827023658937b48161f5a56bb962d7b19999c
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2024-02-15 15:58:58 +0530

    Time: 8 ms (57.40%) | Memory: 45 MB (31.05%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.java
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -1,24 +1,34 @@
 class Solution {
-    public boolean isPalindrom(String s) {
-        int i = 0;
-        int j = s.length() - 1;
-        while (i <= j) {
-            if (s.charAt(i) != s.charAt(j))
-                return false;
-            i++;
-            j--;
-        }
-        return true;
-    }
-
     public int countSubstrings(String s) {
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
         int cnt = 0;
-        for (int i = 0; i < s.length(); i++) {
-            for (int j = 0; j <= i; j++) {
-                if (isPalindrom(s.substring(j, i + 1)))
-                    cnt++;
+
+        // All single characters are palindromes
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = true;
+            cnt++;
+        }
+
+        // Check palindromes of length 2
+        for (int i = 0; i < n - 1; i++) {
+            if (s.charAt(i) == s.charAt(i + 1)) {
+                dp[i][i + 1] = true;
+                cnt++;
             }
         }
+
+        // Check palindromes of length 3 or more
+        for (int len = 3; len <= n; len++) {
+            for (int i = 0; i <= n - len; i++) {
+                int j = i + len - 1;
+                if (dp[i + 1][j - 1] && s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = true;
+                    cnt++;
+                }
+            }
+        }
+
         return cnt;
     }
 }
