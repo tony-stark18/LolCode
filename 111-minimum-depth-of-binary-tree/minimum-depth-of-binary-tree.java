aa5bea7ca0198d053cbcbbe3e870de67428d2c34
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2024-02-18 23:38:28 +0530

    Time: 5 ms (52.98%) | Memory: 63.4 MB (29.34%) - LeetSync

--- 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.java
+++ 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.java
@@ -1,30 +1,11 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    public int soln(TreeNode root,int depth){
-        if(root==null) return Integer.MAX_VALUE;
-        if(root.left==null && root.right==null){
-            return depth;
-        }
-        int lr = soln(root.left,depth+1);
-        int rr = soln(root.right,depth+1);
-        return Math.min(lr,rr);
-    }
     public int minDepth(TreeNode root) {
-        if(root==null) return 0;
-        return soln(root,1);
+        if (root == null) {
+            return 0;
+        }
+
+        int leftDepth = minDepth(root.left);
+        int rightDepth = minDepth(root.right);
+        return (leftDepth == 0 || rightDepth == 0) ? leftDepth + rightDepth + 1 : Math.min(leftDepth, rightDepth) + 1;
     }
 }
