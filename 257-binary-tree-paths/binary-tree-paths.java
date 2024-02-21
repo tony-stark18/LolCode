2baadec689daac5e732d3e8422b06822bee629ad
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2024-02-21 23:18:51 +0530

    Time: 1 ms (99.44%) | Memory: 42.4 MB (76.20%) - LeetSync

--- 257-binary-tree-paths/binary-tree-paths.java
+++ 257-binary-tree-paths/binary-tree-paths.java
@@ -14,22 +14,24 @@
  * }
  */
 class Solution {
-    public void soln(TreeNode root,List<String> ans,String s){
-        if(root==null){
-            return;
-        }
-        if(root.left==null && root.right==null){
-            ans.add(s+Integer.toString(root.val));
-            return;
-        }
-        soln(root.left,ans,s+Integer.toString(root.val)+"->");
-        soln(root.right,ans,s+Integer.toString(root.val)+"->");
-    }
     public List<String> binaryTreePaths(TreeNode root) {
-        List<String> ans = new ArrayList<>();
-        if(root==null) return ans;
-        String s = "";
-        soln(root,ans,s);
-        return ans;
+        List<String> res = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        findPath(res, sb, root);
+        return res;
+    }
+    
+    private void findPath(List<String> res, StringBuilder sb, TreeNode root){
+        if(root == null) return;
+        
+        int len = sb.length();
+        sb.append(root.val);
+
+        if(root.left == null && root.right == null) res.add(sb.toString());
+
+        sb.append("->");
+        findPath(res, sb, root.left);
+        findPath(res, sb, root.right);
+        sb.setLength(len);
     }
 }
