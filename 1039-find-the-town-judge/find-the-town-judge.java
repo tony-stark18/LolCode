a8ceb2593d245c91af6f811fe1f8d341acc5c8a6
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2024-02-22 16:37:12 +0530

    Time: 3 ms (71.04%) | Memory: 53.7 MB (58.06%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.java
+++ 1039-find-the-town-judge/find-the-town-judge.java
@@ -4,21 +4,13 @@ class Solution {
         if(n==1) return 1;
         if(size==0) return -1;
         int arr1[] = new int[n+1];
-        int arr2[] = new int[n+1];
         for(int i=0;i<size;i++){
-            arr1[trust[i][0]]++;
-            arr2[trust[i][1]]++;
+            arr1[trust[i][0]]--;
+            arr1[trust[i][1]]++;
         }
-        int num=0;
-        int cnt =0;
         for(int i=1;i<=n;i++){
-            if(arr1[i]==0 && arr2[i]!=0){
-                num=i;
-                cnt++;   
-            }
+            if(arr1[i]==n-1) return i;
         }
-        if(cnt!=1) return -1;
-        if(arr2[num]==n-1) return num;
         return -1;
     }
 }
