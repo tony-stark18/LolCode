9192d6918663da8ed4a47c32aa41b42c36ca6d1d
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2024-02-26 00:33:31 +0530

    Time: 4 ms (63.04%) | Memory: 13.6 MB (16.50%) - LeetSync

---
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        // Determine which array has a shorter length
+        vector<int>& shorter = (nums1.size() < nums2.size()) ? nums1 : nums2;
+        vector<int>& longer = (nums1.size() < nums2.size()) ? nums2 : nums1;
+
+        // Record each digit and its count in the map
+        unordered_map<int, int> digitCount;
+        for (int num : shorter) {
+            digitCount[num]++;
+        }
+
+        // Iterate over the larger array and check for intersection
+        vector<int> result;
+        for (int num : longer) {
+            if (digitCount.find(num) != digitCount.end() && digitCount[num] > 0) {
+                result.push_back(num);
+                digitCount[num]--;
+                if (digitCount[num] == 0) {
+                    digitCount.erase(num);
+                }
+            }
+        }
+
+        return result;
+    }
+};
