052b47bc31815b228e3d856d82d2ac310ef41a12
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2024-02-26 00:35:06 +0530

    Time: 9 ms (17.27%) | Memory: 13.6 MB (19.47%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
@@ -1,19 +1,30 @@
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        std::unordered_map<int, int> nums1_cnt;
-        for (int num : nums1) {
-            nums1_cnt[num]++;
+        vector<int>& shorter = (nums1.size() < nums2.size()) ? nums1 : nums2;
+        vector<int>& longer = (nums1.size() < nums2.size()) ? nums2 : nums1;
+
+        unordered_map<int, int> digitCount;
+
+        // Record counts for the shorter array
+        for (int num : shorter) {
+            digitCount[num]++;
         }
-        std::vector<int> ans;
-        for (int num : nums2) {
-            if (nums1_cnt.contains(num)) {
-                ans.push_back(num);
-                if (--nums1_cnt[num] == 0) {
-                    nums1_cnt.erase(num);
-                }
+
+        // Iterate over the longer array and check for intersection
+        vector<int> result;
+        for (int num : longer) {
+            if (digitCount.find(num) != digitCount.end() && digitCount[num] > 0) {
+                result.push_back(num);
+                digitCount[num]--;
             }
         }
-        return ans;
+
+        return result;
     }
 };
