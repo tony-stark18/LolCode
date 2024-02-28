9541cd2ebac15f44270867819c5824823e0f9779
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2024-02-28 13:40:28 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (99.42%) - LeetSync

--- 513-find-bottom-left-tree-value/find-bottom-left-tree-value.java
+++ 513-find-bottom-left-tree-value/find-bottom-left-tree-value.java
@@ -1,40 +1,19 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    public int findBottomLeftValue(TreeNode root) {
-        Queue<TreeNode> q = new LinkedList<>();
-        int lv = root.val;
-        q.add(root);
-        q.add(null);
-        while(!q.isEmpty()){
-            TreeNode node = q.remove();
-            if(node==null){
-                if(q.isEmpty()){
-                    break;
-                }
-                else{
-                    lv = q.peek().val;
-                    q.add(null);   
-                }
-            }
-            else{
-                if(node.left!=null) q.add(node.left);
-                if(node.right!=null) q.add(node.right);
-            }
+
+    int leftmost = 0;
+    int leftmostrow = -1;
+    void visit(TreeNode root, int depth) {
+        if (root == null) return;
+        if (depth > leftmostrow) {
+            leftmost = root.val;
+            leftmostrow = depth;
         }
-        return lv;
+        visit(root.left, depth+1);
+        visit(root.right, depth+1);
+    }
+
+    public int findBottomLeftValue(TreeNode root) {
+        visit(root, 0)    ;
+        return leftmost;
     }
 }
