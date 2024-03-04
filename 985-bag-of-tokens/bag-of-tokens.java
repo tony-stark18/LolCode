c46d584ddf9351f8d20c4cdc3fbfaf7324f63686
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2024-03-04 12:49:09 +0530

    Time: 11 ms (5.73%) | Memory: 42.6 MB (57.01%) - LeetSync

--- 985-bag-of-tokens/bag-of-tokens.java
+++ 985-bag-of-tokens/bag-of-tokens.java
@@ -1,26 +1,50 @@
+import java.util.Arrays;
+
 class Solution {
     public int bagOfTokensScore(int[] tokens, int power) {
+        if (tokens.length == 0) {
+            return 0;
+        }
+
         int score = 0;
         int maxScore = 0;
         int left = 0;
         int n = tokens.length;
-        int right = n-1;
-        Arrays.sort(tokens);
-        while(left<=right){
-            while(left<=right && power>=tokens[left]){
-                score++;
-                power-=tokens[left];
-                left++;
-            }
-            if(score==0) return 0;
-            maxScore = Math.max(score,maxScore);
-            if(score>0){
-                score--;
-                power+=tokens[right];
-                right--;
-                maxScore = Math.max(score,maxScore);
+        int right = n - 1;
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (tokens[j] > tokens[j + 1]) {
+                    int temp = tokens[j];
+                    tokens[j] = tokens[j + 1];
+                    tokens[j + 1] = temp;
+                }
             }
         }
+
+        if (power < tokens[left]) {
+            return 0;
+        }
+        
+        while (left <= right) {
+            while (left <= right && power >= tokens[left]) {
+                score++;
+                power -= tokens[left];
+                left++;
+            }
+
+            if (score == 0) return maxScore;
+
+            maxScore = Math.max(score, maxScore);
+
+            if (score > 0) {
+                score--;
+                power += tokens[right];
+                right--;
+                maxScore = Math.max(score, maxScore);
+            }
+        }
+
         return maxScore;
     }
 }
