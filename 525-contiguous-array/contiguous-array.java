2f6730ca6fb9f31055d850900b3249bbc8dbd00d
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2024-03-16 12:21:02 +0530

    Time: 8 ms (98.57%) | Memory: 55.1 MB (42.69%) - LeetSync

--- 525-contiguous-array/contiguous-array.java
+++ 525-contiguous-array/contiguous-array.java
@@ -1,23 +1,21 @@
 class Solution {
     public int findMaxLength(int[] nums) {
-        HashMap<Integer,Integer> hs = new HashMap<>();
-        int cSum = 0;
-        hs.put(0,-1);
-        int max = 0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]>0){
-                cSum++;
-            }
-            else{
-                cSum--;
-            }
-            if(hs.containsKey(cSum)){
-                max = Math.max(max,i-hs.get(cSum));
-            }
-            else{
-                hs.put(cSum,i);
+         int maxLength = 0;
+        int balance = 0;
+        int[] balanceIndices = new int[2 * nums.length + 1]; // To store last occurrence index for each balance value
+        Arrays.fill(balanceIndices, -2); // Initialize with -2 (to handle indices starting from -1)
+        balanceIndices[nums.length] = -1; // Initial balance is 0 at index -1
+        
+        for (int i = 0; i < nums.length; i++) {
+            balance += nums[i] == 0 ? -1 : 1;
+            
+            if (balanceIndices[balance + nums.length] != -2) {
+                maxLength = Math.max(maxLength, i - balanceIndices[balance + nums.length]);
+            } else {
+                balanceIndices[balance + nums.length] = i;
             }
         }
-        return max;
+        
+        return maxLength;
     }
 }
