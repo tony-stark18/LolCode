effbb8aa147c9709beae6947ae9417734ae514a0
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2024-04-02 00:19:46 +0530

    Time: 1 ms (48.22%) | Memory: 40.4 MB (83.59%) - LeetSync

--- 263-ugly-number/ugly-number.java
+++ 263-ugly-number/ugly-number.java
@@ -1,11 +1,4 @@
 class Solution {
-    public boolean isPrime(int n){
-        if(n<2) return false;
-        for(int i=2;i<=Math.sqrt(n);i++){
-            if(n%i==0) return false;
-        }
-        return true;
-    }
     public boolean isUgly(int n) {
         if(n <= 0) return false;
         if(n == 1) return true;
