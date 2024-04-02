72a9168e624ee4a295ece54903b429e2f49d9ff1
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2024-04-02 14:18:46 +0530

    Time: 1 ms (96.36%) | Memory: 45.6 MB (35.02%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,24 +1,22 @@
 class Solution {
     public int soln(int[][] grid,int r,int c,int dp[][]){
-        if(r==grid.length-1 && c==grid[0].length-1){
+        if(r==0 && c==0){
             return grid[r][c];
         }
-        if(r==grid.length || c==grid[0].length) return Integer.MAX_VALUE;
+        if(r<0 || c<0) return Integer.MAX_VALUE;
         if(dp[r][c]!=-1) return dp[r][c];        
-        int right = soln(grid,r,c+1,dp);
-        int down = soln(grid,r+1,c,dp);
+        int left = soln(grid,r,c-1,dp);
+        int up = soln(grid,r-1,c,dp);
         
-        return dp[r][c]=grid[r][c]+Math.min(right,down);
+        return dp[r][c]=grid[r][c]+Math.min(left,up);
     }
     public int minPathSum(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
         int dp[][] = new int[m][n]; 
         for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                dp[i][j]=-1;
-            }
+            Arrays.fill(dp[i],-1);
         }
-        return soln(grid,0,0,dp);
+        return soln(grid,m-1,n-1,dp);
     }
 }
