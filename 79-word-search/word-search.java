b1c21a352a1977ee43dba2f57ce82ed0f6c7d347
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2024-04-03 15:56:00 +0530

    Time: 207 ms (26.23%) | Memory: 45.1 MB (13.18%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,28 +1,31 @@
 class Solution {
-    public boolean soln(char[][] board, String word,int r,int c,int ind,int[][] vis,int dr[],int dc[],int m,int n){
+    public boolean soln(char[][] board, String word, int r, int c, int ind, int m, int n){
         if(ind == word.length()) return true;
-        if(r < 0 || r >= m || c < 0 || c >= n || vis[r][c] == 1 || board[r][c] != word.charAt(ind)) return false;
+        if(r < 0 || r >= m || c < 0 || c >= n || board[r][c] != word.charAt(ind)) return false;
         
-        vis[r][c] = 1;
+        char temp = board[r][c]; // Store the original character
+        board[r][c] = '#'; // Mark the current character as visited
+        
+        // Check in all four directions
+        int[] dr = {-1, 1, 0, 0};
+        int[] dc = {0, 0, -1, 1};
         for(int i = 0; i < 4; i++){
             int newr = r + dr[i];
             int newc = c + dc[i];
-            if(soln(board, word, newr, newc, ind + 1, vis, dr, dc, m, n)) return true;
+            if(soln(board, word, newr, newc, ind + 1, m, n)) return true;
         }
-        vis[r][c] = 0;
+        
+        board[r][c] = temp; // Revert back to the original character
         return false;
     }
     
     public boolean exist(char[][] board, String word) {
         int m = board.length;
         int n = board[0].length;
-        int[][] vis = new int[m][n];
-        int dr[] = {-1, 1, 0, 0};
-        int dc[] = {0, 0, -1, 1};
         
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
-                if(board[i][j] == word.charAt(0) && soln(board, word, i, j, 0, vis, dr, dc, m, n)) return true;
+                if(board[i][j] == word.charAt(0) && soln(board, word, i, j, 0, m, n)) return true;
             }
         }
         return false;
