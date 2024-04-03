6164e2edd6df554709059e2a5924277214ee70a5
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2024-04-03 15:57:15 +0530

    Time: 215 ms (21.97%) | Memory: 45 MB (13.18%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,33 +1,37 @@
 class Solution {
-    public boolean soln(char[][] board, String word, int r, int c, int ind, int m, int n){
-        if(ind == word.length()) return true;
-        if(r < 0 || r >= m || c < 0 || c >= n || board[r][c] != word.charAt(ind)) return false;
-        
-        char temp = board[r][c]; // Store the original character
-        board[r][c] = '#'; // Mark the current character as visited
-        
-        // Check in all four directions
-        int[] dr = {-1, 1, 0, 0};
-        int[] dc = {0, 0, -1, 1};
-        for(int i = 0; i < 4; i++){
-            int newr = r + dr[i];
-            int newc = c + dc[i];
-            if(soln(board, word, newr, newc, ind + 1, m, n)) return true;
-        }
-        
-        board[r][c] = temp; // Revert back to the original character
-        return false;
-    }
-    
     public boolean exist(char[][] board, String word) {
         int m = board.length;
         int n = board[0].length;
         
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                if(board[i][j] == word.charAt(0) && soln(board, word, i, j, 0, m, n)) return true;
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(board[i][j] == word.charAt(0) && dfs(board, word, i, j, 0))
+                    return true;
             }
         }
+        
+        return false;
+    }
+    
+    private boolean dfs(char[][] board, String word, int i, int j, int index) {
+        if(index == word.length()) // Entire word found
+            return true;
+        
+        if(i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != word.charAt(index))
+            return false;
+        
+        char temp = board[i][j];
+        board[i][j] = '#'; // Mark visited
+        
+        // Explore neighbors
+        int[] dr = {-1, 1, 0, 0};
+        int[] dc = {0, 0, -1, 1};
+        for(int k = 0; k < 4; k++) {
+            if(dfs(board, word, i + dr[k], j + dc[k], index + 1))
+                return true;
+        }
+        
+        board[i][j] = temp; // Revert back
         return false;
     }
 }
