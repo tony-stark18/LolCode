e6a0e785a58a6defaed86f8eb30c8aff30285988
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2024-04-03 19:28:49 +0530

    Time: 1 ms (99.10%) | Memory: 44.2 MB (36.30%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -1,27 +1,21 @@
 class Solution {
-    public int soln(List<List<Integer>> triangle, int i, int j, int[][] dp) {
-        if (i == triangle.size()-1) return triangle.get(i).get(j);
-        if (dp[i][j] != -1) return dp[i][j];
-        int res1 = triangle.get(i).get(j) + soln(triangle, i + 1, j, dp);
-        int res2 = triangle.get(i).get(j) + soln(triangle, i + 1, j+ 1, dp);
-        return dp[i][j] = Math.min(res1, res2);
+
+    public int solve(int i, int j, int n, List<List<Integer>> triangle, int dp[][]){
+        if(dp[i][j] != -1)return dp[i][j];
+        if(i==n)return dp[i][j]=0;
+
+        return dp[i][j]=triangle.get(i).get(j) + Math.min(solve(i+1, j, n, triangle, dp), solve(i+1, j+1, n, triangle, dp));     
     }
 
     public int minimumTotal(List<List<Integer>> triangle) {
         int n = triangle.size();
-        int[][] dp = new int[n][n];
-        // for(int i=0;i<n;i++){
-        //     Arrays.fill(dp[i],-1);
-        // }
-        // return soln(triangle, 0, 0, dp);
-        for(int i=0;i<triangle.get(n-1).size();i++){
-            dp[n-1][i]=triangle.get(n-1).get(i);
+        int m = triangle.get(n-1).size();
+        int dp[][] = new int[n+1][m+1];
+        
+        for(int arr[] : dp){
+            Arrays.fill(arr, -1);
         }
-        for(int i=n-2;i>=0;i--){
-            for(int j=triangle.get(i).size()-1;j>=0;j--){
-                dp[i][j]=Math.min(dp[i+1][j],dp[i+1][j+1])+triangle.get(i).get(j);
-            }
-        }
-        return dp[0][0];
+
+        return solve(0, 0, n, triangle, dp);
     }
 }
