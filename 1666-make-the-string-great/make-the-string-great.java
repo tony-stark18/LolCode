acadeb3601f5f627e8f1ce9a0cc473162f249b60
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2024-04-05 14:23:21 +0530

    Time: 2 ms (88.33%) | Memory: 41.8 MB (92.18%) - LeetSync

--- 1666-make-the-string-great/make-the-string-great.java
+++ 1666-make-the-string-great/make-the-string-great.java
@@ -1,15 +1,14 @@
 class Solution {
     public String makeGood(String s) {
-        if (s.length() <= 1) return s;
+        if(s.length()==1) return s;
         StringBuilder sb = new StringBuilder(s);
         boolean flag = true;
-        while (flag) {
+        while(flag){
             flag = false;
-            for (int i = 0; i < sb.length() - 1; i++) {
-                if (Math.abs((int) sb.charAt(i) - (int) sb.charAt(i + 1)) == 32) {
-                    sb.delete(i, i + 2);
-                    flag = true;
-                    break;
+            for(int i=0;i<sb.length()-1;i++){
+                if(Math.abs((int)sb.charAt(i)-(int)sb.charAt(i+1))==32){
+                    sb=sb.delete(i,i+2);
+                    flag =true;
                 }
             }
         }
