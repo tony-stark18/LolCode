9c1dada39614dbbbcf4b588ae98b9208f14ee63c
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2024-04-05 14:45:12 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (21.31%) - LeetSync

--- 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.java
+++ 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.java
@@ -1,26 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    private int count = 0;
-    
-    private int[] calculateSumAndCount(TreeNode root) {
-        if (root == null) {
-            return new int[]{0, 0};
+    public int count = 0;
+    public int[] soln(TreeNode root){
+        if(root==null){
+            return new int[2];
         }
-        
-        int[] left = calculateSumAndCount(root.left);
-        int[] right = calculateSumAndCount(root.right);
-        
-        int sum = left[0] + right[0] + root.val;
-        int totalCount = left[1] + right[1] + 1;
-        
-        if (sum / totalCount == root.val) {
-            count++;
-        }
-        
-        return new int[]{sum, totalCount};
+        int[] arr1 = soln(root.left);
+        int[] arr2 = soln(root.right);
+        int sum=arr1[0]+arr2[0]+root.val;
+        int nodes=arr1[1]+arr2[1]+1;
+        if(sum/nodes==root.val) count++;
+        return new int[]{sum,nodes};
     }
-    
     public int averageOfSubtree(TreeNode root) {
-        calculateSumAndCount(root);
+        soln(root);
         return count;
     }
 }
