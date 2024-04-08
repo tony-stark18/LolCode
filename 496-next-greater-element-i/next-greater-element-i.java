8651eef21731710a77c95e42d71c6b127c9ae772
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2024-04-08 15:50:02 +0530

    Time: 4 ms (45.49%) | Memory: 44.1 MB (35.01%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,18 +1,18 @@
 class Solution {
-    public int soln(int[] nums2,int i){
-        for(int itr=i;itr<nums2.length;itr++){
-            if(nums2[itr]>nums2[i]) return nums2[itr];
-        }
-        return -1;
-    }
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int[] ans = new int[nums1.length];
+        HashMap<Integer,Integer> hs = new HashMap<>();
+        Stack<Integer> st = new Stack<>();
+        for(int num:nums2){
+            while(!st.isEmpty() && st.peek()<num){
+                hs.put(st.pop(),num);
+            }
+            st.push(num);
+        }
+        int ans[] = new int[nums1.length];
+        Arrays.fill(ans,-1);
         for(int i=0;i<nums1.length;i++){
-            for(int j=0;j<nums2.length;j++){
-                if(nums2[j]==nums1[i]){
-                    ans[i]=soln(nums2,j);
-                    break;
-                }
+            if(hs.containsKey(nums1[i])){
+                ans[i]=hs.get(nums1[i]);
             }
         }
         return ans;
