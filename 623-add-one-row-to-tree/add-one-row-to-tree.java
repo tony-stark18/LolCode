7305fd5b370c661c55a7f0c3700efd4964a013f4
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2024-04-16 10:21:24 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (9.38%) - LeetSync

--- 623-add-one-row-to-tree/add-one-row-to-tree.java
+++ 623-add-one-row-to-tree/add-one-row-to-tree.java
@@ -1,39 +1,41 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
-    public void dfs(TreeNode root,int val,int depthG,int depthR){
-        if(root==null) return;
-        if(depthR==depthG-1){
-            TreeNode ln = root.left;
-            TreeNode rn = root.right;
+    public void soln(TreeNode root, int val, int depth, int gdepth) {
+        if (root == null)
+            return;
+        if (depth == gdepth - 1) {
+            TreeNode lst = root.left;
+            TreeNode rst = root.right;
             root.left = new TreeNode(val);
             root.right = new TreeNode(val);
-            root.left.left = ln;
-            root.right.right = rn;
+            root.left.left = lst;
+            root.right.right = rst;
         }
-        dfs(root.left,val,depthG,depthR+1);
-        dfs(root.right,val,depthG,depthR+1);
+        soln(root.left, val, depth + 1, gdepth);
+        soln(root.right, val, depth + 1, gdepth);
     }
+
     public TreeNode addOneRow(TreeNode root, int val, int depth) {
         if(depth==1){
             TreeNode newNode = new TreeNode(val);
             newNode.left=root;
             return newNode;
         }
-        dfs(root,val,depth,1);
+        soln(root, val, 1, depth);
         return root;
     }
 }
