d794399ae2aa867500db704d13f998fcd75760f0
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2024-06-02 10:37:57 +0530

    Time: 26 ms (72.62%) | Memory: 16.8 MB (39.83%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,33 +1,30 @@
 class Solution {
 public:
-    int numIslands(vector<vector<char>>& grid) {
-        if (grid.empty() || grid[0].empty()) {
-            return 0;
-        }
+    void dfs(vector<vector<char>>& grid, vector<vector<int>>& vis, int r, int c, int m, int n) {
+        vis[r][c] = 1;
+        int directions[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
         
-        int numIslands = 0;
-        for (int i = 0; i < grid.size(); i++) {
-            for (int j = 0; j < grid[0].size(); j++) {
-                if (grid[i][j] == '1') {
-                    numIslands++;
-                    dfs(grid, i, j);
+        for (int k = 0; k < 4; ++k) {
+            int nr = r + directions[k][0];
+            int nc = c + directions[k][1];
+            if (nr >= 0 && nr < m && nc >= 0 && nc < n && !vis[nr][nc] && grid[nr][nc] == '1') {
+                dfs(grid, vis, nr, nc, m, n);
+            }
+        }
+    }
+    int numIslands(vector<vector<char>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int>> vis(m,vector<int>(n,0));
+        int cnt =0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]=='1' && vis[i][j]==0){
+                    cnt++;
+                    dfs(grid,vis,i,j,m,n);
                 }
             }
         }
-        
-        return numIslands;
-    }
-    
-private:
-    void dfs(vector<vector<char>>& grid, int i, int j) {
-        if (i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size() || grid[i][j] != '1') {
-            return;
-        }
-        
-        grid[i][j] = '0'; // mark as visited
-        dfs(grid, i + 1, j); // down
-        dfs(grid, i - 1, j); // up
-        dfs(grid, i, j + 1); // right
-        dfs(grid, i, j - 1); // left
+        return cnt;
     }
 };
