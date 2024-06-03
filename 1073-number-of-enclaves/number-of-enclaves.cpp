a8dd7dd687ea73f81498b9cfadf4e623a889eb08
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2024-06-03 13:00:37 +0530

    Time: 36 ms (98.94%) | Memory: 30.2 MB (81.18%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.cpp
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -1,49 +1,59 @@
 class Solution {
 public:
-    void dfs(int r,int c,vector<vector<int>>& grid,vector<vector<int>>& vis,int dr[],int dc[],int m,int n){
-        vis[r][c]=1;
-        for(int i=0;i<4;i++){
-            int nr = r+dr[i];
-            int nc = c+dc[i];
-            if(nr>=0 && nc>=0 && nr<m && nc<n && !vis[nr][nc] && grid[nr][nc]==1){
-                dfs(nr,nc,grid,vis,dr,dc,m,n);
+    void dfs(int r, int c, vector<vector<int>>& grid, int dr[], int dc[], int m, int n) {
+        stack<pair<int, int>> stk;
+        stk.push({r, c});
+        grid[r][c] = 0;  // Mark as visited by setting it to 0
+        
+        while (!stk.empty()) {
+            auto [cr, cc] = stk.top();
+            stk.pop();
+            
+            for (int i = 0; i < 4; i++) {
+                int nr = cr + dr[i];
+                int nc = cc + dc[i];
+                if (nr >= 0 && nc >= 0 && nr < m && nc < n && grid[nr][nc] == 1) {
+                    grid[nr][nc] = 0;  // Mark as visited
+                    stk.push({nr, nc});
+                }
             }
         }
     }
-    int numEnclaves(vector<vector<int>>& board) {
-        int m = board.size();
-        int n = board[0].size();
-        vector<vector<int>> vis(m,vector<int>(n,0));
-        int dr[] = {-1,1,0,0};
-        int dc[] = {0,0,-1,1};
-        for(int i=0;i<m;i++){
-            if(board[i][0]==1){
-                vis[i][0]=1;
-                dfs(i,0,board,vis,dr,dc,m,n);
+
+    int numEnclaves(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        int dr[] = {-1, 1, 0, 0};
+        int dc[] = {0, 0, -1, 1};
+
+        // Perform DFS for all boundary cells with value 1
+        for (int i = 0; i < m; i++) {
+            if (grid[i][0] == 1) {
+                dfs(i, 0, grid, dr, dc, m, n);
             }
-            if(board[i][n-1]==1){
-                vis[i][n-1]=1;
-                dfs(i,n-1,board,vis,dr,dc,m,n);
-            }
-        }
-        for(int j=0;j<n;j++){
-            if(board[0][j]==1){
-                vis[0][j]=1;
-                dfs(0,j,board,vis,dr,dc,m,n);
-            }
-            if(board[m-1][j]==1){
-                vis[m-1][j]=1;
-                dfs(m-1,j,board,vis,dr,dc,m,n);
+            if (grid[i][n - 1] == 1) {
+                dfs(i, n - 1, grid, dr, dc, m, n);
             }
         }
-        int count =0;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(board[i][j]==1 && !vis[i][j]){
+        for (int j = 0; j < n; j++) {
+            if (grid[0][j] == 1) {
+                dfs(0, j, grid, dr, dc, m, n);
+            }
+            if (grid[m - 1][j] == 1) {
+                dfs(m - 1, j, grid, dr, dc, m, n);
+            }
+        }
+
+        // Count all remaining 1s in the grid
+        int count = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
                     count++;
                 }
             }
         }
+
         return count;
     }
 };
