6fc12f97731dbaebb9cdf9b8a20ed24d30d8ba50
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2024-06-04 15:15:46 +0530

    Updated Solution - GfG to GitHub

--- Medium/Detect cycle in a directed graph/detect-cycle-in-a-directed-graph.cpp
+++ Medium/Detect cycle in a directed graph/detect-cycle-in-a-directed-graph.cpp
@@ -4,28 +4,25 @@ using namespace std;
 
 // } Driver Code Ends
 class Solution {
-  private:
-    bool dfs(int start, vector<int> adj[], int vis[],int pvis[]){
-        vis[start] = 1;
-        pvis[start] = 1;
-        for(auto it:adj[start]){
-            if(!vis[it]){
-                if(dfs(it,adj,vis,pvis)) return true;
-            }
-            else if(pvis[it]) return true;
-        }
-        pvis[start] = 0;
-        return false;
-    }
   public:
     // Function to detect cycle in a directed graph.
-    bool isCyclic(int V, vector<int> adj[]) {
-        int vis[V] = {0};
-        int pvis[V] = {0};
-        for(int i=0;i<V;i++){
-            if(!vis[i]){
-                if(dfs(i,adj,vis,pvis)) return true;
+    bool dfs(vector<int> adj[],vector<int> &vis,vector<int> &pvis,int node){
+        vis[node]=1;
+        pvis[node]=1;
+        for(auto it:adj[node]){
+            if(!vis[it]){
+                if(dfs(adj,vis,pvis,it)) return true;
             }
+            else if(pvis[it]==1) return true;
+        }
+        pvis[node]=0;
+        return false;
+    }
+    bool isCyclic(int V, vector<int> adj[]) {
+        vector<int> vis(V,0);
+        vector<int> pvis(V,0);
+        for(int i=0;i<V;i++){
+            if(dfs(adj,vis,pvis,i)) return true;
         }
         return false;
     }
