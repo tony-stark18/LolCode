8421c0648b95e2e4f0c36739506bb5ac97df8977
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2024-06-06 11:15:24 +0530

    Time: 24 ms (92.93%) | Memory: 44.5 MB (99.46%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.java
+++ 876-hand-of-straights/hand-of-straights.java
@@ -5,22 +5,19 @@ class Solution {
         if (hand.length % groupSize != 0) return false;
 
         Arrays.sort(hand);
-        Map<Integer, Integer> countMap = new HashMap<>();
-        for (int card : hand) {
-            countMap.put(card, countMap.getOrDefault(card, 0) + 1);
+        Map<Integer, Integer> hs = new HashMap<>();
+        for(int i:hand){
+            hs.put(i,hs.getOrDefault(i,0)+1);
         }
-
-        for (int card : hand) {
-            if (countMap.get(card) == 0) continue;
-
-            for (int i = 0; i < groupSize; i++) {
-                int currentCard = card + i;
-                if (countMap.getOrDefault(currentCard, 0) == 0) {
-                    return false;
-                }
-                countMap.put(currentCard, countMap.get(currentCard) - 1);
+        for(int i:hand){
+            if(hs.get(i)==0) continue;
+            for(int j=0;j<groupSize;j++){
+                int curr = i+j;
+                if(hs.getOrDefault(curr,0)==0) return false;
+                hs.put(curr,hs.get(curr)-1);
             }
         }
         return true;
+
     }
 }
