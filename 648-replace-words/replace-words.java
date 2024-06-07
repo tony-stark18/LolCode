82b560b5cf8a4d11a2b7768c3cbd8cb35de39e6b
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2024-06-07 11:39:13 +0530

    Time: 10 ms (88.52%) | Memory: 55.7 MB (32.54%) - LeetSync

--- 648-replace-words/replace-words.java
+++ 648-replace-words/replace-words.java
@@ -1,27 +1,68 @@
-import java.util.HashSet;
-import java.util.List;
 
 class Solution {
-    public String replaceWords(List<String> dictionary, String sentence) {
-        HashSet<String> dict = new HashSet<>(dictionary);
-        String st[] = sentence.split(" ");
-        StringBuilder ans = new StringBuilder();
-        for (String word : st) {
-            StringBuilder sb = new StringBuilder();
-            boolean found = false;
+    class TrieNode {
+        boolean isEnd;
+        TrieNode[] children;
+        
+        TrieNode() {
+            isEnd = false;
+            children = new TrieNode[26];
+        }
+    }
+
+    class Trie {
+        TrieNode root;
+        
+        Trie() {
+            root = new TrieNode();
+        }
+        
+        void insert(String word) {
+            TrieNode node = root;
             for (char c : word.toCharArray()) {
-                sb.append(c);
-                if (dict.contains(sb.toString())) {
-                    ans.append(sb).append(" ");
-                    found = true;
+                int index = c - 'a';
+                if (node.children[index] == null) {
+                    node.children[index] = new TrieNode();
+                }
+                node = node.children[index];
+            }
+            node.isEnd = true;
+        }
+        
+        String getShortestRoot(String word) {
+            TrieNode node = root;
+            StringBuilder prefix = new StringBuilder();
+            for (char c : word.toCharArray()) {
+                int index = c - 'a';
+                if (node.children[index] == null) {
                     break;
                 }
+                node = node.children[index];
+                prefix.append(c);
+                if (node.isEnd) {
+                    return prefix.toString();
+                }
             }
-            if (!found) {
-                ans.append(word).append(" ");
-            }
+            return word;
         }
-        // Remove the trailing space
-        return ans.toString().trim();
+    }
+
+    public String replaceWords(List<String> dictionary, String sentence) {
+        Trie trie = new Trie();
+        for (String root : dictionary) {
+            trie.insert(root);
+        }
+
+        String[] words = sentence.split(" ");
+        StringBuilder ans = new StringBuilder();
+
+        for (String word : words) {
+            if (ans.length() > 0) {
+                ans.append(" ");
+            }
+            ans.append(trie.getShortestRoot(word));
+        }
+
+        return ans.toString();
     }
 }
