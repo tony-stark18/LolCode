df68d28ccb8a595a40547e007233d9245fcaf407
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2024-06-12 17:30:03 +0530

    Time: 17 ms (83.03%) | Memory: 58 MB (43.59%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
@@ -1,33 +1,45 @@
 class Solution {
-    public int helper(int[] arr,int k,int day){
+    // Helper function to calculate the number of bouquets that can be made by `day`
+    public int helper(int[] arr, int k, int day) {
         int count = 0;
         int bcount = 0;
-        for(int i:arr){
-            if(i<=day){
+        for (int i : arr) {
+            if (i <= day) {
                 count++;
-            }
-            else{
-                bcount+=(count/k);
-                count=0;
+                if (count == k) {
+                    bcount++;
+                    count = 0;
+                }
+            } else {
+                count = 0;
             }
         }
-        bcount+=count/k;
         return bcount;
     }
+
     public int minDays(int[] bloomDay, int m, int k) {
-        int n=bloomDay.length;
-        if((long)m*k>(long)n) return -1;
-        int minDay = bloomDay[0];
-        int maxDay = bloomDay[0];
-        for(int i:bloomDay){
-            minDay=Math.min(minDay,i);
-            maxDay=Math.max(maxDay,i);
+        int n = bloomDay.length;
+        if ((long)m * k > (long)n) return -1;
+
+        int minDay = Integer.MAX_VALUE;
+        int maxDay = Integer.MIN_VALUE;
+
+        // Determine the minimum and maximum bloom days
+        for (int i : bloomDay) {
+            minDay = Math.min(minDay, i);
+            maxDay = Math.max(maxDay, i);
         }
-        while(minDay<=maxDay){
-            int midDay = (minDay+maxDay)/2;
-            int bcount = helper(bloomDay,k,midDay);
-            if(bcount>=m) maxDay=midDay-1;
-            else minDay = midDay+1;
+
+        // Binary search for the minimum number of days
+        while (minDay <= maxDay) {
+            int midDay = minDay + (maxDay - minDay) / 2;
+            int bcount = helper(bloomDay, k, midDay);
+
+            if (bcount >= m) {
+                maxDay = midDay - 1;
+            } else {
+                minDay = midDay + 1;
+            }
         }
         return minDay;
     }
