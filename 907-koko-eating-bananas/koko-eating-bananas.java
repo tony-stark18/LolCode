529f05c1f61703ca51a3256a486c12cedc47583a
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2024-06-12 16:46:18 +0530

    Time: 12 ms (84.70%) | Memory: 44.6 MB (95.29%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,30 +1,26 @@
 class Solution {
-    public int getHours(int[] piles,int n){
-        int total_hour=0;
-        for(int i=0;i<piles.length;i++){
-            total_hour+=Math.ceil((double)piles[i]/(double)n);
-        }
-        return total_hour;
-    }
     public int minEatingSpeed(int[] piles, int h) {
-        int max=piles[0];
-        for(int i:piles){
-            max=Math.max(max,i);
-        }
-        int low=1;
-        int high=max;
-        while(low<=high){
-            int mid = (low+high)/2;
-            int hour = getHours(piles,mid);
-            System.out.println(hour);
-            if(hour<=h){
-                high=mid-1;
-            }
-            else if(hour>h){
-                low=mid+1;
+        int left = 1;
+        int right = 1000000000;
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            if(totalTime(piles, mid) <= h){
+                right = mid - 1;
+            }else{
+                left = mid + 1;
             }
         }
-        return low;
+        return left;
 
+    }
+    private long totalTime(int[] piles, int speed){
+        long time = 0;
+        for(int i = 0; i < piles.length; i++){
+            time += piles[i] / speed;
+            if(piles[i] % speed > 0){
+                time++;
+            } 
+        }
+        return time;
     }
 }
