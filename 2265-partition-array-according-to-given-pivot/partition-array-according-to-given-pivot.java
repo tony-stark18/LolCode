dc37c5c66fb49452b687c577ee2265652a510623
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2024-06-14 15:50:29 +0530

    Time: 489 ms (5.01%) | Memory: 68.2 MB (21.40%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
@@ -1,33 +1,23 @@
-
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
-        List<Integer> less = new ArrayList<>();
-        List<Integer> equal = new ArrayList<>();
-        List<Integer> greater = new ArrayList<>();
-        
-        for (int num : nums) {
-            if (num < pivot) {
-                less.add(num);
-            } else if (num == pivot) {
-                equal.add(num);
-            } else {
-                greater.add(num);
+        int n = nums.length;
+        int arr[] = new int[n];
+        int k = 0;
+        for(int i=0;i<n;i++){
+            if(nums[i]<pivot){
+                arr[k++]=nums[i];
             }
         }
-        
-        int[] result = new int[nums.length];
-        int k = 0;
-        
-        for (int num : less) {
-            result[k++] = num;
+        for(int i=0;i<n;i++){
+            if(nums[i]==pivot){
+                arr[k++]=nums[i];
+            }
         }
-        for (int num : equal) {
-            result[k++] = num;
+        for(int i=0;i<n;i++){
+            if(nums[i]>pivot){
+                arr[k++]=nums[i];
+            }
         }
-        for (int num : greater) {
-            result[k++] = num;
-        }
-        
-        return result;
+        return arr;
     }
 }
