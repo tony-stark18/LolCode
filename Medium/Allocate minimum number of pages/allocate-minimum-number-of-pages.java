caf886e751fcaeb1b45b50840867318cd4de1377
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2024-06-14 13:39:08 +0530

    Updated Solution - GfG to GitHub

--- Medium/Allocate minimum number of pages/allocate-minimum-number-of-pages.java
+++ Medium/Allocate minimum number of pages/allocate-minimum-number-of-pages.java
@@ -1,69 +1,68 @@
 //{ Driver Code Starts
-//Initial Template for Java
+// Initial Template for Java
 
 /*package whatever //do not write package name here */
 
 import java.io.*;
 import java.util.*;
+
 class GFG {
-	public static void main (String[] args) {
-		Scanner sc=new Scanner(System.in);
-		
-		int t=sc.nextInt();
-		
-		while(t-->0)
-		{
-		    int n=sc.nextInt();
-		    int a[]=new int[n];
-		    
-		    for(int i=0;i<n;i++)
-		    {
-		        a[i]=sc.nextInt();
-		    }
-		    int m=sc.nextInt();
-		    Solution ob = new Solution();
-		    System.out.println(ob.findPages(a,n,m));
-		}
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int a[] = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                a[i] = sc.nextInt();
+            }
+            int m = sc.nextInt();
+            Solution ob = new Solution();
+            System.out.println(ob.findPages(n, a, m));
+        }
+    }
 }
 // } Driver Code Ends
 
 
-//User function Template for Java
+// User function Template for Java
 
-class Solution 
-{
-    public static int helper(int[] A,int pages){
-        int stu = 1;
-        int pg  = 0;
+class Solution {
+    // Function to find minimum number of pages.
+    public static int helper(int pages,int[] A){
+        int stu=0;
+        int pg = 0;
         for(int i:A){
             if(pg+i<=pages){
-                pg = pg+i;
+                pg+=i;
             }
             else{
                 stu++;
-                pg = i;
+                pg=i;
             }
         }
-        return stu;
+        return stu+1;
     }
-    //Function to find minimum number of pages.
-    public static int findPages(int[]A,int N,int M)
-    {
-        if(N<M) return -1;
-        int high = 0;
+    public static long findPages(int N, int[] A, int M) {
+        if(M>N) return -1;
         int low = 0;
+        int high = 0;
         for(int i:A){
+            low=Math.max(low,i);
             high+=i;
-            low = Math.max(i,low);
         }
         while(low<=high){
             int mid = (low+high)/2;
-            int stu = helper(A,mid);
+            int stu = helper(mid,A);
             if(stu<=M){
-                high = mid-1;
+                high=mid-1;
             }
-            else low = mid+1;
+            else{
+                low = mid+1;
+            }
         }
         return low;
     }
