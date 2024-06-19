6f0e79a5596d2f8ac088f1df4e91274150125159
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2024-06-19 23:50:24 +0530

    Time: 105 ms (83.62%) | Memory: 68.9 MB (65.37%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,28 +1,37 @@
 class Solution {
 public:
-    int helper(vector<int>& bloomDay,int day,int k){
+    int helper(vector<int>& bloomDay, int day, int k) {
         int bqt = 0;
         int flower = 0;
-        for(int i:bloomDay){
-            if(i<=day) flower++;
-            else {
-                bqt+=flower/k;
-                flower=0;
+        for (int i : bloomDay) {
+            if (i <= day) {
+                flower++;
+                if (flower == k) {
+                    bqt++;
+                    flower = 0;
+                }
+            } else {
+                flower = 0;
             }
         }
-        bqt+=flower/k;
         return bqt;
     }
+
     int minDays(vector<int>& bloomDay, int m, int k) {
-        int low = *(min_element(bloomDay.begin(), bloomDay.end()));
-        int high = *(max_element(bloomDay.begin(), bloomDay.end()));
-        if(helper(bloomDay,high,k)<m) return -1;
-        while(low<=high){
-            int mid = low+(high-low)/2;
-            int bqt = helper(bloomDay,mid,k);
-            if(bqt>=m) high = mid-1;
-            else low = mid+1;
+        int low = *min_element(bloomDay.begin(), bloomDay.end());
+        int high = *max_element(bloomDay.begin(), bloomDay.end());
+
+        if (helper(bloomDay, high, k) < m) return -1;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (helper(bloomDay, mid, k) >= m) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
         }
+
         return low;
     }
 };
