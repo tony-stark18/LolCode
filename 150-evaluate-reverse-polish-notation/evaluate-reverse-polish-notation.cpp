784b78e00631ca2568eef374d14396a18aed2a4f
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2024-06-20 22:18:25 +0530

    Time: 3 ms (97.03%) | Memory: 15.5 MB (55.43%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
@@ -2,19 +2,18 @@ class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
         stack<int> st;
-        for(string it:tokens){
-            if(it=="+" || it=="-" || it=="*" || it=="/"){
-                int num1= st.top();
-                st.pop();
-                int num2=st.top();
-                st.pop();
-                if(it=="+") st.push(num1+num2);
-                else if(it=="-") st.push(num2-num1);
-                else if(it=="*") st.push(num1*num2);
-                else if(it=="/") st.push(num2/num1);
-            }
-            else{
-                st.push(stoi(it));
+        for (const auto& token : tokens) {
+            if (token == "+" || token == "-" || token == "*" || token == "/") {
+                int num1 = st.top(); st.pop();
+                int num2 = st.top(); st.pop();
+                
+                if (token == "+") st.push(num2 + num1);
+                else if (token == "-") st.push(num2 - num1);
+                else if (token == "*") st.push(num2 * num1);
+                else if (token == "/") st.push(num2 / num1);
+                
+            } else {
+                st.push(stoi(token));
             }
         }
         return st.top();
