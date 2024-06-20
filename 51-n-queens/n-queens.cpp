405eccdf159c27960f42084d42c3b35800308952
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2024-06-20 15:29:30 +0530

    Time: 8 ms (31.57%) | Memory: 10.5 MB (42.03%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,61 +1,31 @@
 class Solution {
 public:
-    bool canPlace(vector<string>& board, int r, int c, int n) {
-        // row
-        int row = r;
-        int col = c;
-        while (col >= 0) {
-            if (board[row][col] == 'Q')
-                return false;
-            col--;
-        }
-        // col
-        row = r;
-        col = c;
-        while (row >= 0) {
-            if (board[row][col] == 'Q')
-                return false;
-            row--;
-        }
-
-        // diaog-1
-        row = r;
-        col = c;
-        while (row >= 0 && col >= 0) {
-            if (board[row][col] == 'Q')
-                return false;
-            row--;
-            col--;
-        }
-        // diaog-2
-        row = r;
-        col = c;
-        while (row < n && col >= 0) {
-            if (board[row][col] == 'Q')
-                return false;
-            row++;
-            col--;
-        }
-        return true;
-    }
-    void soln(vector<vector<string>>& ans, vector<string>& board, int col,
-              int n) {
+    void soln(vector<vector<string>>& ans, vector<string>& board, vector<bool>& rows, vector<bool>& mainDiag, vector<bool>& antiDiag, int col, int n) {
         if (col == n) {
             ans.push_back(board);
             return;
         }
         for (int i = 0; i < n; i++) {
-            if (canPlace(board, i, col, n)) {
-                board[i][col] = 'Q';
-                soln(ans, board, col + 1, n);
-                board[i][col] = '.';
-            }
+            if (rows[i] || mainDiag[i - col + n - 1] || antiDiag[i + col])
+                continue;
+            // Place the queen
+            board[i][col] = 'Q';
+            rows[i] = mainDiag[i - col + n - 1] = antiDiag[i + col] = true;
+            // Recur to place the rest
+            soln(ans, board, rows, mainDiag, antiDiag, col + 1, n);
+            // Remove the queen and backtrack
+            board[i][col] = '.';
+            rows[i] = mainDiag[i - col + n - 1] = antiDiag[i + col] = false;
         }
     }
+    
     vector<vector<string>> solveNQueens(int n) {
         vector<string> board(n, string(n, '.'));
         vector<vector<string>> ans;
-        soln(ans, board, 0, n);
+        vector<bool> rows(n, false);
+        vector<bool> mainDiag(2 * n - 1, false); // main diagonal
+        vector<bool> antiDiag(2 * n - 1, false); // anti-diagonal
+        soln(ans, board, rows, mainDiag, antiDiag, 0, n);
         return ans;
     }
 };
