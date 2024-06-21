46f6956a98e076bb63ac346da5c6f2966b627243
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2024-06-21 15:26:28 +0530

    Time: 8 ms (79.41%) | Memory: 32.3 MB (20.38%) - LeetSync

--- 2662-check-knight-tour-configuration/check-knight-tour-configuration.cpp
+++ 2662-check-knight-tour-configuration/check-knight-tour-configuration.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    bool soln(vector<vector<int>>& grid,int r,int c,int n,vector<vector<int>> dir){
-        if(grid[r][c]==n*n-1) return true;
-        bool flag = false;
-        for(int i=0;i<8;i++){
-            int nr = r+dir[i][0];
-            int nc = c+dir[i][1];
+    bool soln(const vector<vector<int>>& grid, int r, int c, int n, const vector<vector<int>>& dir) {
+        if (grid[r][c] == n * n - 1) return true;
+        for (const auto& d : dir) {
+            int nr = r + d[0];
+            int nc = c + d[1];
             int prev = grid[r][c];
-            if(nr>=0 && nr<n && nc>=0 && nc<n && grid[nr][nc]==prev+1){
-                if(soln(grid,nr,nc,n,dir)){
-                    flag = true;
+            if (nr >= 0 && nr < n && nc >= 0 && nc < n && grid[nr][nc] == prev + 1) {
+                if (soln(grid, nr, nc, n, dir)) {
+                    return true;
                 }
             }
         }
-        return flag ;
+        return false;
     }
+
     bool checkValidGrid(vector<vector<int>>& grid) {
-        if(grid[0][0]!=0) return false;
-        vector<vector<int>> dir = {{1,2},{1,-2},{-1,2},{-1,-2},{2,1},{2,-1},{-2,1},{-2,-1}};
-        return soln(grid,0,0,grid.size(),dir);
+        if (grid[0][0] != 0) return false;
+        static const vector<vector<int>> dir = {{1, 2}, {1, -2}, {-1, 2}, {-1, -2}, {2, 1}, {2, -1}, {-2, 1}, {-2, -1}};
+        return soln(grid, 0, 0, grid.size(), dir);
     }
 };
