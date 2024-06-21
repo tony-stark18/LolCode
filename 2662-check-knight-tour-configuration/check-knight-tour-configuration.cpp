44133eae94cf986f6ab0b5b76d5a537e0eaeb701
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2024-06-21 15:27:14 +0530

    Time: 12 ms (53.36%) | Memory: 31.9 MB (26.68%) - LeetSync

--- 2662-check-knight-tour-configuration/check-knight-tour-configuration.cpp
+++ 2662-check-knight-tour-configuration/check-knight-tour-configuration.cpp
@@ -1,23 +1,57 @@
 class Solution {
 public:
-    bool soln(const vector<vector<int>>& grid, int r, int c, int n, const vector<vector<int>>& dir) {
-        if (grid[r][c] == n * n - 1) return true;
-        for (const auto& d : dir) {
-            int nr = r + d[0];
-            int nc = c + d[1];
-            int prev = grid[r][c];
-            if (nr >= 0 && nr < n && nc >= 0 && nc < n && grid[nr][nc] == prev + 1) {
-                if (soln(grid, nr, nc, n, dir)) {
-                    return true;
-                }
+    bool IsValid(int row, int col, int n){
+        if(row>=0 && row<n && col>=0 && col<n)return true;
+        return false;
+    }
+    bool solve(vector<vector<int>>& grid, vector<vector<int>>& vis,
+    vector<vector<int>>& res, int n, int step, int row, int col){
+        if(step==(n*n))return true;
+        if(IsValid(row, col, n) && !vis[row][col]){
+            vis[row][col] = 1;
+            res[row][col] = step;
+            // int dir[] = {-2, -1, 1, 2};
+            if(solve(grid, vis, res, n, step+1, row-1, col+2))return true;
+            if(solve(grid, vis, res, n, step+1, row+1, col+2))return true;
+            if(solve(grid, vis, res, n, step+1, row-2, col+1))return true;
+            if(solve(grid, vis, res, n, step+1, row+2, col+1))return true;
+            if(solve(grid, vis, res, n, step+1, row-2, col-1))return true;
+            if(solve(grid, vis, res, n, step+1, row+2, col-1))return true;
+            if(solve(grid, vis, res, n, step+1, row-1, col-2))return true;
+            if(solve(grid, vis, res, n, step+1, row+1, col-2))return true;
+            vis[row][col] = 0;
+            res[row][col] = -1;
+        }
+        return false;
+    }
+    bool check(vector<vector<int>>& grid, int r, int c, int n, int step){
+        if(!IsValid(r, c, n))return false;
+        return grid[r][c]==step ? true : false;
+    }
+    bool dfs(int r, int c, int n, vector<vector<int>>& grid, 
+    vector<int>& row, vector<int>& col, int step){
+        if(step==(n*n)-1)return true;
+        for(int i=0;i<8;i++){
+            if(check(grid, r+row[i], c+col[i], n, step+1)){
+                return dfs(r+row[i], c+col[i], n, grid, row, col, step+1);
             }
         }
         return false;
     }
-
     bool checkValidGrid(vector<vector<int>>& grid) {
-        if (grid[0][0] != 0) return false;
-        static const vector<vector<int>> dir = {{1, 2}, {1, -2}, {-1, 2}, {-1, -2}, {2, 1}, {2, -1}, {-2, 1}, {-2, -1}};
-        return soln(grid, 0, 0, grid.size(), dir);
+        int n = grid.size();
+        vector<int>row = {-2, -2, -1, -1, 1, 1, 2, 2};
+        vector<int>col = {-1, 1, -2, 2, -2, 2, -1, 1};
+        if(grid[0][0]!=0)return false;
+        // for(int i=0;i<n;i++){
+            // for(int j=0;i<n;i++){
+                // if(grid[i][j]==0)
+                return dfs(0, 0, n, grid, row, col, 0);
+            // }
+        // }
+        // vector<vector<int>> vis(n, vector<int>(n, 0));
+        // vector<vector<int>> res(n, vector<int>(n, -1));
+        // return solve(grid, vis, res, n, 0, 0, 0);
+        return false;
     }
 };
