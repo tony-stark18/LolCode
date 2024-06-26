31bb3d54c7075810a2f0a272b1d1c9976cdb0968
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2024-06-26 14:07:33 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (65.67%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -1,31 +1,13 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    public void inorder(TreeNode root,List<Integer> ls){
-       if(root==null) return;
-        inorder(root.left,ls);
-        ls.add(root.val);
-        inorder(root.right,ls);
-    }
+       
     public boolean isValidBST(TreeNode root) {
-        List<Integer> ls = new ArrayList<>();
-        inorder(root,ls);
-        for(int i=1;i<ls.size();i++){
-            if(ls.get(i)<=ls.get(i-1)) return false;
-        }   
-        return true;
+        return check(root, Long.MIN_VALUE, Long.MAX_VALUE);
     }
+
+    private boolean check(TreeNode root, long minValue, long maxValue) {
+        if (root == null) return true;
+        if (root.val <= minValue || root.val >= maxValue) return false;
+        return check(root.left, minValue, root.val) && check(root.right, root.val, maxValue);
+    }
+    
 }
