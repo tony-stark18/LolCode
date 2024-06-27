acb64cae522bdb2abaed2d1f51ad2a20cf505c3e
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2024-06-27 23:20:42 +0530

    Time: 5 ms (100.00%) | Memory: 44.9 MB (53.95%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
@@ -9,20 +9,18 @@
  */
 
 class Solution {
-    public boolean soln(TreeNode root,TreeNode p,TreeNode q,TreeNode[] ans){
-        if(root==null) return false;
-        boolean self = root.val==p.val || root.val==q.val;
-        boolean left = soln(root.left,p,q,ans);
-        boolean right = soln(root.right,p,q,ans);
-
-        if((self && left) || (self && right) || (left && right)){
-            ans[0]=root;
+    public TreeNode soln(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null)
+            return null;
+        if (p.val < root.val && q.val < root.val) {
+            return soln(root.left, p, q);
+        } else if (p.val > root.val && q.val > root.val) {
+            return soln(root.right, p, q);
+        } else {
+            return root;
         }
-        return self || left || right;
     }
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        TreeNode ans[] = {null};
-        soln(root,p,q,ans);
-        return ans[0];
+        return soln(root,p,q);
     }
 }
