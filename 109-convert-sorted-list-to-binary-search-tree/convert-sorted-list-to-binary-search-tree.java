00a3598b33aa9eae89425f9704fe6b97ba926188
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2024-06-28 13:44:39 +0530

    Time: 18 ms (5.08%) | Memory: 44.6 MB (70.74%) - LeetSync

--- 109-convert-sorted-list-to-binary-search-tree/convert-sorted-list-to-binary-search-tree.java
+++ 109-convert-sorted-list-to-binary-search-tree/convert-sorted-list-to-binary-search-tree.java
@@ -24,23 +24,28 @@
  * }
  */
 class Solution {
-    public TreeNode construct(List<Integer> ls,int left,int right){
-        if(left>right) return null;
-        if(left==right) return new TreeNode(ls.get(left));
-        int mid =(left+right)/2;
-        TreeNode root = new TreeNode(ls.get(mid));
-        root.left = construct(ls,left,mid-1);
-        root.right = construct(ls,mid+1,right);
+    public ListNode getMid(ListNode head){
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while(fast!=null && fast.next!=null && fast.next.next!=null){
+            slow=slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+    public TreeNode construct(ListNode head){
+        if(head==null) return null;
+        if(head.next==null) return new TreeNode(head.val);
+        ListNode mid = getMid(head);
+        TreeNode root = new TreeNode(mid.next.val);
+        System.out.println(mid.val);
+        ListNode rightHead = mid.next.next;
+        mid.next = null;
+        root.left = construct(head);
+        root.right = construct(rightHead);
         return root;
     }
     public TreeNode sortedListToBST(ListNode head) {
-        List<Integer> ls = new ArrayList<>();
-        while(head!=null){
-            ls.add(head.val);
-            head = head.next;
-        }
-        int left = 0;
-        int right = ls.size()-1;
-        return construct(ls,left,right);
+        return construct(head);
     }
 }
