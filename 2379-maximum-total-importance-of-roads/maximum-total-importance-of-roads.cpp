9b12e42b14bd567689211ef2b4a52cacc6b996fa
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2024-06-28 11:52:07 +0530

    Time: 302 ms (56.55%) | Memory: 126.9 MB (65.35%) - LeetSync

--- 2379-maximum-total-importance-of-roads/maximum-total-importance-of-roads.cpp
+++ 2379-maximum-total-importance-of-roads/maximum-total-importance-of-roads.cpp
@@ -1,24 +1,31 @@
 class Solution {
 public:
     long long maximumImportance(int n, vector<vector<int>>& roads) {
-        priority_queue<pair<int,int>> pq;
-        vector<int> nodes(n,0);
-        for(auto it:roads){
-            nodes[it[0]]++;
-            nodes[it[1]]++;
+        vector<int> degrees(n, 0);
+        
+        // Count the degree of each node
+        for (const auto& road : roads) {
+            degrees[road[0]]++;
+            degrees[road[1]]++;
         }
-        for(int i=0;i<n;i++){
-            pq.push({nodes[i],i});
+        
+        // Create a vector of pairs (degree, node) and sort it by degree in descending order
+        vector<pair<int, int>> degree_node;
+        for (int i = 0; i < n; ++i) {
+            degree_node.push_back({degrees[i], i});
         }
-        while(!pq.empty()){
-            auto pair = pq.top();
-            pq.pop();
-            nodes[pair.second]=n--;
+        sort(degree_node.rbegin(), degree_node.rend());
+        
+        // Assign importance values to nodes based on their degree
+        vector<int> importance(n, 0);
+        for (int i = 0; i < n; ++i) {
+            importance[degree_node[i].second] = n - i;
         }
         long long ans = 0;
-        for(auto it:roads){
-            ans+=nodes[it[0]]+nodes[it[1]];
+        for (const auto& road : roads) {
+            ans += importance[road[0]] + importance[road[1]];
         }
+        
         return ans;
     }
 };
