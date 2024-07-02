a0cf75eb75a4f9bd15db51c1d96eb2e0c4f5aa03
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2024-07-02 23:40:26 +0530

    Time: 44 ms (53.48%) | Memory: 44.7 MB (79.59%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,47 +1,27 @@
 class Solution {
-    public boolean canPartition(int[] arr) {
-        int n = arr.length;
-        int totSum = 0;
-        for (int i = 0; i < n; i++) {
-            totSum += arr[i];
-        }
-        if (totSum % 2 == 1)
+    public boolean canPartition(int[] nums) {
+        int sum = 0;
+        for (int i : nums)
+            sum += i;
+        if (sum % 2 != 0)
             return false;
-        else {
-            int k = totSum / 2;
-            boolean prev[] = new boolean[k + 1];
-
-            // Initialize the first row of the DP table
-            prev[0] = true;
-
-            // Initialize the first column of the DP table
-            if (arr[0] <= k) {
-                prev[arr[0]] = true;
-            }
-
-            // Fill in the DP table using bottom-up dynamic programming
-            for (int ind = 1; ind < n; ind++) {
-                boolean cur[] = new boolean[k + 1];
-                cur[0] = true;
-                for (int target = 1; target <= k; target++) {
-                    // Calculate if the current element is not taken
-                    boolean notTaken = prev[target];
-
-                    // Calculate if the current element is taken
-                    boolean taken = false;
-                    if (arr[ind] <= target) {
-                        taken = prev[target - arr[ind]];
-                    }
-
-                    // Update the DP table for the current element and target sum
-                    cur[target] = notTaken || taken;
-                }
-                // Update the previous row with the current row for the next iteration
-                prev = cur;
-            }
-
-            // The result is stored in the last cell of the DP table
-            return prev[k];
+        int target = sum / 2;
+        boolean dp[][] = new boolean[nums.length][target + 1];
+        for (int i = 0; i < nums.length; i++) {
+            dp[i][0] = true;
         }
+
+        for (int i = 1; i <nums.length; i++) {
+            for (int j = 1; j <= target; j++) {
+                int currEl = nums[i];
+                if (currEl <= j) {
+                    dp[i][j] = dp[i-1][j] || dp[i-1][j - currEl];
+                } else {
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return dp[nums.length-1][target];
+
     }
 }
