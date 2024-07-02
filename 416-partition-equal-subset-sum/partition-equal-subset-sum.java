3410eb881a9dc9bbdff4dffe56fdc71a5b45d5b5
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2024-07-02 22:42:16 +0530

    Time: 33 ms (72.37%) | Memory: 44.7 MB (79.59%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,45 +1,25 @@
+import java.util.Arrays;
+
 class Solution {
-    // public boolean soln(int[] nums,int target,int ind,int dp[][]){
-    // if(target==0) return true;
-    // if(ind==0) return nums[0]==target;
-    // if(dp[ind][target]!=-1) return (dp[ind][target]==1)?true:false;
-    // boolean nTake = soln(nums,target,ind-1,dp);
-    // boolean take=false;
-    // if(nums[ind]<=target) take = soln(nums,target-nums[ind],ind-1,dp);
-    // dp[ind][target]= (nTake || take)?1:0;
-    // return nTake || take;
-    // }
     public boolean canPartition(int[] nums) {
         int sum = 0;
-        for (int i : nums)
-            sum += i;
-        if (sum % 2 != 0)
-            return false;
-        // int dp[][] = new int[nums.length][(sum/2)+1];
-        // for(int i=0;i<nums.length;i++){
-        // Arrays.fill(dp[i],-1);
-        // }
-        // return soln(nums,sum/2,nums.length-1,dp);
+        for (int num : nums) sum += num;
+
+        // If the total sum is odd, it's not possible to partition into two equal subsets
+        if (sum % 2 != 0) return false;
+
         int target = sum / 2;
-        boolean dp[][] = new boolean[nums.length][target + 1];
-        for (int i = 0; i < nums.length; i++) {
-            dp[i][0] = true;
-        }
-        for(int i=0;i<=target;i++){
-            if(i==nums[0]) dp[0][i]=true;
-        }
+        boolean[] dp = new boolean[target + 1];
+        dp[0] = true; 
 
-        for (int i = 1; i <nums.length; i++) {
-            for (int j = 1; j <= target; j++) {
-                int currEl = nums[i];
-                if (currEl <= j) {
-                    dp[i][j] = dp[i-1][j] || dp[i-1][j - currEl];
-                } else {
-                    dp[i][j] = dp[i-1][j];
-                }
+        for (int num : nums) {
+            boolean[] curr = Arrays.copyOf(dp, target + 1); 
+            for (int j = num; j <= target; j++) {
+                curr[j] = dp[j] || dp[j - num];
             }
+            dp = curr;
         }
-        return dp[nums.length-1][target];
 
+        return dp[target];
     }
 }
