ac3e5136efb70ec8a1e535f8821f0cfd5c9a9a78
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2024-07-03 18:21:39 +0530

    Time: 7 ms (49.29%) | Memory: 47.5 MB (53.55%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -11,10 +11,24 @@ class Solution {
         return dp[ind][amount] = take + nTake;
     }
     public int change(int amount, int[] coins) {
+        // int dp[][] = new int[coins.length][amount+1];
+        // for(int i=0;i<coins.length;i++){
+        //     Arrays.fill(dp[i],-1);
+        // }
+        // return soln(amount,coins,coins.length-1,dp);
         int dp[][] = new int[coins.length][amount+1];
-        for(int i=0;i<coins.length;i++){
-            Arrays.fill(dp[i],-1);
+        for (int i = 0; i <= amount; i++) {
+            if (i % coins[0] == 0)
+                dp[0][i] = 1;
         }
-        return soln(amount,coins,coins.length-1,dp);
+        for(int ind=1;ind<coins.length;ind++){
+            for(int tar=0;tar<=amount;tar++){
+                int take = 0;
+                int nTake = dp[ind-1][tar];
+                if(coins[ind]<=tar) take = dp[ind][tar-coins[ind]];
+                dp[ind][tar] = take+nTake;
+            }
+        }
+        return dp[coins.length-1][amount];
     }
 }
