9500bc48113b7d0a5a1aeadc3daffd0eabe2d293
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2024-07-03 13:02:07 +0530

    Time: 2 ms (42.69%) | Memory: 7.8 MB (17.05%) - LeetSync

---
+++ 62-unique-paths/unique-paths.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int soln(int m,int n,vector<vector<int>> &dp){
+        if(m==0 && n==0) return 1;
+        if(m<0 || n<0) return 0;
+        if(dp[m][n]!=-1) return dp[m][n];
+        return dp[m][n]=soln(m-1,n,dp)+soln(m,n-1,dp);
+    }
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        return soln(m-1,n-1,dp);
+    }
+};
