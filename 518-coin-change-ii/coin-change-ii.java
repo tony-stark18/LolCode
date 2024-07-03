c1f1093a31c0d6fd2d048972579521105aaa6099
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2024-07-03 18:24:39 +0530

    Time: 2 ms (100.00%) | Memory: 40.9 MB (89.85%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,34 +1,12 @@
 class Solution {
-    public int soln(int amount,int coins[],int ind,int dp[][]){
-        if(amount==0) return 1;
-        if(ind<0) return 0;
-        if(dp[ind][amount]!=-1) return dp[ind][amount];
-        int nTake = soln(amount,coins,ind-1,dp);
-        int take = 0;
-        if(coins[ind]<=amount){
-            take = soln(amount-coins[ind],coins,ind,dp);
-        }
-        return dp[ind][amount] = take + nTake;
-    }
     public int change(int amount, int[] coins) {
-        // int dp[][] = new int[coins.length][amount+1];
-        // for(int i=0;i<coins.length;i++){
-        //     Arrays.fill(dp[i],-1);
-        // }
-        // return soln(amount,coins,coins.length-1,dp);
-        int dp[][] = new int[coins.length][amount+1];
-        for (int i = 0; i <= amount; i++) {
-            if (i % coins[0] == 0)
-                dp[0][i] = 1;
-        }
-        for(int ind=1;ind<coins.length;ind++){
-            for(int tar=0;tar<=amount;tar++){
-                int take = 0;
-                int nTake = dp[ind-1][tar];
-                if(coins[ind]<=tar) take = dp[ind][tar-coins[ind]];
-                dp[ind][tar] = take+nTake;
+        int[] dp = new int[amount+1];
+        dp[0]=1;
+        for(int coin:coins){
+            for(int j=coin; j<=amount; j++){
+                dp[j] = dp[j]+dp[j-coin];
             }
         }
-        return dp[coins.length-1][amount];
+        return dp[amount];
     }
 }
