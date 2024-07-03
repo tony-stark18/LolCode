323fbac4c9a0c40d24568b29369806c5b3e3bd3d
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2024-07-03 15:24:43 +0530

    Time: 103 ms (46.56%) | Memory: 45.8 MB (6.23%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,11 +1,15 @@
 class Solution {
-    public int soln(int[] nums,int target,int ind){
+    public int soln(int[] nums,int target,int ind,HashMap<String,Integer> dp){
+        String key = ind + "," + target;
+        if(dp.containsKey(key)) return dp.get(key);
         if(ind<0) return target==0?1:0;
-        int plus = soln(nums,target-nums[ind],ind-1);
-        int minus = soln(nums,target+nums[ind],ind-1);
+        int plus = soln(nums,target-nums[ind],ind-1,dp);
+        int minus = soln(nums,target+nums[ind],ind-1,dp);
+        dp.put(key,plus+minus);
         return plus+minus;
     }
     public int findTargetSumWays(int[] nums, int target) {
-        return soln(nums,target,nums.length-1);
+        HashMap<String,Integer> dp = new HashMap<>();
+        return soln(nums,target,nums.length-1,dp);
     }
 }
