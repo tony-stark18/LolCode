6bb4c164756fa0879ec046f2bd2f133afb6f3805
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2024-07-04 23:25:48 +0530

    Time: 23 ms (40.28%) | Memory: 50.6 MB (70.56%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,17 +1,17 @@
 class Solution {
-    public int soln(char text1[],char text2[],int i,int j,int dp[][]){
-        if(i==text1.length || j==text2.length) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(text1[i]==text2[j]){
-            return dp[i][j] = 1+soln(text1,text2,i+1,j+1,dp);
-        }
-        return dp[i][j]=Math.max(soln(text1,text2,i+1,j,dp),soln(text1,text2,i,j+1,dp));
+    public int soln(char[] arr1,char[] arr2,int ind1,int ind2,int dp[][]){
+        if(ind1<0 || ind2<0) return 0;
+        if(dp[ind1][ind2]!=-1) return dp[ind1][ind2];
+        if(arr1[ind1]==arr2[ind2]) return dp[ind1][ind2] = 1+soln(arr1,arr2,ind1-1,ind2-1,dp);
+        return dp[ind1][ind2] = Math.max(soln(arr1,arr2,ind1-1,ind2,dp),soln(arr1,arr2,ind1,ind2-1,dp));
     }
     public int longestCommonSubsequence(String text1, String text2) {
-        int dp[][] = new int[text1.length()][text2.length()];
-        for(int i=0;i<text1.length();i++){
+        char[] arr1 = text1.toCharArray();
+        char[] arr2 = text2.toCharArray();
+        int dp[][] = new int[arr1.length][arr2.length];
+        for(int i=0;i<arr1.length;i++){
             Arrays.fill(dp[i],-1);
         }
-        return soln(text1.toCharArray(),text2.toCharArray(),0,0,dp);
+        return soln(arr1,arr2,arr1.length-1,arr2.length-1,dp);
     }
 }
