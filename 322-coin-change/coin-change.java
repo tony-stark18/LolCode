0470fd0bbfc00294c5c7554a095034c6db327e7e
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2024-07-04 14:40:52 +0530

    Time: 16 ms (61.55%) | Memory: 44.9 MB (29.07%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,22 +1,37 @@
 class Solution {
-    public int soln(int ind,int[] coins,int amount,int dp[][]){
-        if(ind==0){
-            if(amount%coins[0]==0) return amount/coins[0];
-            return (int)1e9;
+    public int soln(int ind, int[] coins, int amount, int dp[][]) {
+        if (ind == 0) {
+            if (amount % coins[0] == 0)
+                return amount / coins[0];
+            return (int) 1e9;
         }
-        if(dp[ind][amount]!=-1) return dp[ind][amount];
-        int nTake = 0+soln(ind-1,coins,amount,dp);
+        if (dp[ind][amount] != -1)
+            return dp[ind][amount];
+        int nTake = 0 + soln(ind - 1, coins, amount, dp);
         int take = Integer.MAX_VALUE;
-        if(amount>=coins[ind]) take = 1+soln(ind,coins,amount-coins[ind],dp);
-        return dp[ind][amount]=Math.min(take,nTake);
+        if (amount >= coins[ind])
+            take = 1 + soln(ind, coins, amount - coins[ind], dp);
+        return dp[ind][amount] = Math.min(take, nTake);
     }
+
     public int coinChange(int[] coins, int amount) {
-        int[][] dp = new int[coins.length][amount+1];
-        for(int i=0;i<coins.length;i++){
-            Arrays.fill(dp[i],-1);
+        int[][] dp = new int[coins.length][amount + 1];
+        for (int i = 0; i <= amount; i++) {
+            if (i % coins[0] == 0)
+                dp[0][i] = i / coins[0];
+            else
+                dp[0][i] = (int) 1e9;
         }
-        int ans = soln(coins.length-1,coins,amount,dp);
-        if(ans>=1e9) return -1;
-        return ans;
+        for (int i = 1; i < coins.length; i++) {
+            for (int j = 0; j <= amount; j++) {
+                int nTake = dp[i - 1][j];
+                int take = Integer.MAX_VALUE;
+                if (coins[i]<=j)
+                    take = 1 + dp[i][j - coins[i]];
+                dp[i][j] = Math.min(take, nTake);
+            }
+        }
+        if(dp[coins.length-1][amount]>=1e9) return -1;
+        return dp[coins.length-1][amount];
     }
 }
