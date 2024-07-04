37f1b5a4b274ad00bca6aae963e407f8c5ad0623
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2024-07-04 12:06:19 +0530

    Time: 5 ms (84.77%) | Memory: 81.6 MB (68.31%) - LeetSync

--- 2299-merge-nodes-in-between-zeros/merge-nodes-in-between-zeros.java
+++ 2299-merge-nodes-in-between-zeros/merge-nodes-in-between-zeros.java
@@ -10,27 +10,24 @@
  */
 class Solution {
     public ListNode mergeNodes(ListNode head) {
-        ListNode dummyHead = head;
-        ListNode t = dummyHead;
-        ListNode temp = head.next;
-        dummyHead.next = null;
+        ListNode dummyHead = new ListNode(0); // Create a dummy node to hold the new list
+        ListNode t = dummyHead; // Pointer to build the new list
+        ListNode temp = head.next; // Skip the initial 0
 
-        while(temp!=null){
-            int sum = 0;
-            while(temp!=null){
-                if(temp.next.val==0){
-                    temp.next.val = sum+temp.val;
-                    t.next = temp.next;
-                    temp.next = null;
-                    t=t.next;
-                    temp = t.next;
-                    t.next=null;
-                    break;
-                }
-                sum+=temp.val;
-                temp = temp.next;
+        int sum = 0; // Initialize sum to accumulate values
+
+        while (temp != null) {
+            if (temp.val == 0) {
+                // When we encounter a 0, we create a new node with the sum of the previous segment
+                t.next = new ListNode(sum);
+                t = t.next; // Move the pointer to the new node
+                sum = 0; // Reset the sum for the next segment
+            } else {
+                // Accumulate the sum of the current segment
+                sum += temp.val;
             }
+            temp = temp.next; // Move to the next node
         }
-        return dummyHead.next;
+        return dummyHead.next; // Return the next node of the dummy head, which is the start of the new list
     }
 }
