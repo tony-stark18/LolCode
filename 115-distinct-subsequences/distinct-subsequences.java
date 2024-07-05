0226b4c13151c0a839ed4541125ce5cdc723b2a9
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2024-07-05 23:30:16 +0530

    Time: 6 ms (96.55%) | Memory: 48.7 MB (62.62%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -14,10 +14,23 @@ class Solution {
     public int numDistinct(String s, String t) {
         char arr1[] = s.toCharArray();
         char arr2[] = t.toCharArray();
-        int dp[][] = new int[arr1.length][arr2.length];
+        // int dp[][] = new int[arr1.length][arr2.length];
+        // for (int i = 0; i < arr1.length; i++) {
+        // Arrays.fill(dp[i], -1);
+        // }
+        // return soln(arr1, arr2, arr1.length - 1, arr2.length - 1, dp);
+        int dp[][] = new int[arr1.length + 1][arr2.length + 1];
         for (int i = 0; i < arr1.length; i++) {
-            Arrays.fill(dp[i], -1);
+            dp[i][0] = 1;
         }
-        return soln(arr1, arr2, arr1.length - 1, arr2.length - 1, dp);
+        for (int i = 1; i <= arr1.length; i++) {
+            for (int j = 1; j <= arr2.length; j++) {
+                if (arr1[i-1] == arr2[j-1])
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                else
+                    dp[i][j] = dp[i - 1][j];
+            }
+        }
+        return dp[arr1.length][arr2.length];
     }
 }
