ecab1aa5d291a7497b671104ad68ed15025ac0ce
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2024-07-07 19:41:38 +0530

    Time: 0 ms (100.00%) | Memory: 40 MB (68.63%) - LeetSync

--- 1642-water-bottles/water-bottles.java
+++ 1642-water-bottles/water-bottles.java
@@ -1,10 +1,5 @@
 class Solution {
     public int numWaterBottles(int n, int e) {
-        int ans = n;
-        while(n>=e){
-            ans += n/e;
-            n = (n/e)+n%e;
-        }
-        return ans;
+        return n + (n - 1) / (e - 1);
     }
 }
