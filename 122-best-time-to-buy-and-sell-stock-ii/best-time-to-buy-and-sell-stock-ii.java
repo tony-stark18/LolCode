8860909d6a477206985298fbe472c5eff9229431
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2024-07-15 17:03:12 +0530

    Time: 2 ms (15.49%) | Memory: 45.7 MB (45.56%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -20,21 +20,24 @@ class Solution {
 
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        int[][] dp = new int[n + 1][2];
+        // int[][] dp = new int[n + 1][2];
+        int[] prev = new int[2];
+        int[] curr = new int[2];
         // for (int[] row : dp) Arrays.fill(row, -1);
         for (int ind = n - 1; ind >= 0; ind--) {
             for (int holding = 0; holding <= 1; holding++) {
                 if (holding == 0) {
-                    int buy = -prices[ind] + dp[ind + 1][1];
-                    int skip = dp[ind + 1][0];
-                    dp[ind][holding] = Math.max(buy, skip);
+                    int buy = -prices[ind] + prev[1];
+                    int skip = prev[0];
+                    curr[holding] = Math.max(buy, skip);
                 } else {
-                    int sell = prices[ind] + dp[ind + 1][0];
-                    int hold = dp[ind + 1][1];
-                    dp[ind][holding] = Math.max(sell, hold);
+                    int sell = prices[ind] + prev[0];
+                    int hold = prev[1];
+                    curr[holding] = Math.max(sell, hold);
                 }
             }
+            prev = curr.clone();
         }
-        return dp[0][0];
+        return prev[0];
     }
 }
