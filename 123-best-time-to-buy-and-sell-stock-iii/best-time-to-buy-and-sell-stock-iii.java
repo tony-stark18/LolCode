eb645d94fa09389514eed130b4d9201c4d061c33
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2024-07-15 22:59:30 +0530

    Time: 46 ms (47.92%) | Memory: 60.4 MB (81.39%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
@@ -1,24 +1,37 @@
 class Solution {
-    public int f(int ind, int hcap, int prices[],int [][] dp) {
+    public int f(int ind, int hcap, int prices[], int[][] dp) {
         if (ind == prices.length || hcap == 4)
             return 0;
-        if(dp[ind][hcap]!=-1) return dp[ind][hcap];
-        if (hcap%2 == 0) {
-            int buy = -prices[ind] + f(ind + 1,hcap+1, prices,dp);
-            int skip = 0 + f(ind + 1, hcap, prices,dp);
+        if (dp[ind][hcap] != -1)
+            return dp[ind][hcap];
+        if (hcap % 2 == 0) {
+            int buy = -prices[ind] + f(ind + 1, hcap + 1, prices, dp);
+            int skip = 0 + f(ind + 1, hcap, prices, dp);
             return dp[ind][hcap] = Math.max(buy, skip);
         } else {
-            int sell = prices[ind] + f(ind + 1, hcap + 1, prices,dp);
-            int nSell = f(ind + 1, hcap, prices,dp);
+            int sell = prices[ind] + f(ind + 1, hcap + 1, prices, dp);
+            int nSell = f(ind + 1, hcap, prices, dp);
             return dp[ind][hcap] = Math.max(sell, nSell);
         }
     }
 
     public int maxProfit(int[] prices) {
-        int [][] dp= new int[prices.length][4];
-        for(int i=0;i<prices.length;i++){
-            Arrays.fill(dp[i],-1);
+        int[] after = new int[5];
+        int[] curr = new int[5];
+        for (int ind = prices.length - 1; ind >= 0; ind--) {
+            for (int hcap = 3; hcap >= 0; hcap--) {
+                if (hcap % 2 == 0) {
+                    int buy = -prices[ind] + after[hcap + 1];
+                    int skip = after[hcap];
+                    curr[hcap] = Math.max(buy, skip);
+                } else {
+                    int sell = prices[ind] + after[hcap + 1];
+                    int nSell = after[hcap];
+                    curr[hcap] = Math.max(sell, nSell);
+                }
+            }
+            after = curr.clone();
         }
-        return f(0,0,prices,dp);
+        return after[0];
     }
 }
