add39afd54114688fc6110cfc7aaed55f4f2721c
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2024-07-18 14:01:12 +0530

    Time: 8 ms (60.17%) | Memory: 44.8 MB (41.19%) - LeetSync

---
+++ 1653-number-of-good-leaf-nodes-pairs/number-of-good-leaf-nodes-pairs.java
@@ -0,0 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    //post-order
+    public List<Integer> soln(TreeNode root,int d,int ans[]){
+        if(root==null) return new ArrayList<>();
+        if(root.left==null && root.right==null){
+            List<Integer> res = new ArrayList<>();
+            res.add(1);
+            return res;
+        }
+        List<Integer> ls = soln(root.left,d,ans);
+        List<Integer> rs = soln(root.right,d,ans);
+        // System.out.println(ls.size()+" "+rs.size());
+        // for(int i:ls) System.out.print(i);
+        // System.out.println();
+        // for(int j:rs) System.out.print(j);
+        for(int i=0;i<ls.size();i++){
+            for(int j=0;j<rs.size();j++){
+                if(ls.get(i)+rs.get(j)<=d)
+                    ans[0]+=1;
+            }
+        }
+        List<Integer> res = new ArrayList<>();
+        for(int i=0;i<ls.size();i++){
+            if(ls.get(i)+1<d){
+                res.add(ls.get(i)+1);
+            }
+        }
+        for(int i=0;i<rs.size();i++){
+            if(rs.get(i)+1<d){
+                res.add(rs.get(i)+1);
+            }
+        }
+        return res; 
+    }
+    public int countPairs(TreeNode root, int distance) {
+        int ans[] = new int[1];
+        soln(root,distance,ans);
+        return ans[0];
+    }
+}
