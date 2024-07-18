20d363458df9569e2fb3dac93a1767cd9501828e
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2024-07-18 14:30:38 +0530

    Time: 7 ms (63.05%) | Memory: 44.7 MB (55.42%) - LeetSync

--- 1653-number-of-good-leaf-nodes-pairs/number-of-good-leaf-nodes-pairs.java
+++ 1653-number-of-good-leaf-nodes-pairs/number-of-good-leaf-nodes-pairs.java
@@ -5,7 +5,7 @@
  *     TreeNode left;
  *     TreeNode right;
  *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val) { this.val = val; }1
  *     TreeNode(int val, TreeNode left, TreeNode right) {
  *         this.val = val;
  *         this.left = left;
@@ -14,40 +14,38 @@
  * }
  */
 class Solution {
-    // Post-order traversal to count pairs
-    public int[] dfs(TreeNode root, int distance, int[] ans) {
-        if (root == null) return new int[distance + 1];
-        if (root.left == null && root.right == null) {
-            int[] leafDistances = new int[distance + 1];
-            leafDistances[1] = 1; // Distance of 1 from the leaf itself
-            return leafDistances;
+    //post-order
+    public List<Integer> soln(TreeNode root,int d,int ans[]){
+        if(root==null) return new ArrayList<>();
+        if(root.left==null && root.right==null){
+            List<Integer> res = new ArrayList<>();
+            res.add(1);
+            return res;
         }
-
-        int[] leftDistances = dfs(root.left, distance, ans);
-        int[] rightDistances = dfs(root.right, distance, ans);
-
-        // Count pairs
-        for (int i = 1; i <= distance; i++) {
-            for (int j = 1; j <= distance; j++) {
-                if (i + j <= distance) {
-                    ans[0] += leftDistances[i] * rightDistances[j];
-                }
+        List<Integer> ls = soln(root.left,d,ans);
+        List<Integer> rs = soln(root.right,d,ans);
+        for(int i=0;i<ls.size();i++){
+            for(int j=0;j<rs.size();j++){
+                if(ls.get(i)+rs.get(j)<=d)
+                    ans[0]+=1;
             }
         }
-
-        // Aggregate distances for the current node
-        int[] currentDistances = new int[distance + 1];
-        for (int i = 1; i < distance; i++) {
-            currentDistances[i + 1] += leftDistances[i];
-            currentDistances[i + 1] += rightDistances[i];
+        List<Integer> res = new ArrayList<>();
+        for(int i=0;i<ls.size();i++){
+            if(ls.get(i)+1<d){
+                res.add(ls.get(i)+1);
+            }
         }
-
-        return currentDistances;
+        for(int i=0;i<rs.size();i++){
+            if(rs.get(i)+1<d){
+                res.add(rs.get(i)+1);
+            }
+        }
+        return res; 
     }
-
     public int countPairs(TreeNode root, int distance) {
-        int[] ans = new int[1];
-        dfs(root, distance, ans);
+        int ans[] = new int[1];
+        soln(root,distance,ans);
         return ans[0];
     }
 }
