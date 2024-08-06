8df3921dd6f51e163bd2cb1f386cc974418d6c64
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2024-08-06 11:02:22 +0530

    Time: 72 ms (62.04%) | Memory: 24.8 MB (66.15%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
@@ -1,18 +1,26 @@
 class Solution {
 public:
     int minimumPushes(string word) {
-        int freq[26];
-        for(char c:word){
-            freq[c-'a']++;
+        int freq[26] = {0};
+        
+        // Count the frequency of each character
+        for(char c : word) {
+            freq[c - 'a']++;
         }
-        sort(freq,freq+26);
+        
+        // Sort frequencies in ascending order
+        sort(freq, freq + 26);
+        
         int chars = 0;
         int press = 0;
-        for(int i=25;i>=0;i--){
-            if(freq[i]==0) break;
-            press = press+((chars/8)+1)*freq[i];
+        
+        // Calculate minimum presses needed
+        for(int i = 25; i >= 0; i--) {
+            if(freq[i] == 0) break;
+            press += ((chars / 8) + 1) * freq[i];
             chars++;
         }
+        
         return press;
     }
 };
