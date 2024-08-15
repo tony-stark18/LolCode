d71b82c547b74f2beea61d963f24b7182f0e5891
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2024-08-15 22:14:12 +0530

    Time: 71 ms (62.76%) | Memory: 86.1 MB (9.36%) - LeetSync

--- 890-lemonade-change/lemonade-change.cpp
+++ 890-lemonade-change/lemonade-change.cpp
@@ -1,36 +1,25 @@
 class Solution {
 public:
     bool lemonadeChange(vector<int>& bills) {
-        if(bills[0]!=5) return false;
-        int i=0;
-        while(i<bills.size() && bills[i]==5){
-            i++;
-        }
-        int Fvs = i;
-        int Tens = 0;
-        // int Tws = 0;
-        while(i<bills.size()){
-            if(bills[i]==5){
-                Fvs++;
-            }
-            else if(bills[i]==10){
-                if(Fvs==0) return false;
-                Fvs--;
-                Tens++;
-            }
-            else{
-                if(Tens>0 && Fvs>0){
-                    Fvs--;
-                    Tens--;
-                }
-                else if(Tens==0 && Fvs>2){
-                    Fvs-=3;
-                }
-                else{
+        int fives = 0, tens = 0;
+        
+        for (int bill : bills) {
+            if (bill == 5) {
+                fives++;
+            } else if (bill == 10) {
+                if (fives == 0) return false;
+                fives--;
+                tens++;
+            } else { 
+                if (tens > 0 && fives > 0) {
+                    tens--;
+                    fives--;
+                } else if (fives >= 3) {
+                    fives -= 3;
+                } else {
                     return false;
                 }
             }
-            i++;
         }
         return true;
     }
