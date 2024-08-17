d47b5d21e8d4dbe8ac8d99d20f320531f85e2563
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2024-08-17 12:13:32 +0530

    Time: 23 ms (98.97%) | Memory: 43.6 MB (26.33%) - LeetSync

--- 1063-best-sightseeing-pair/best-sightseeing-pair.cpp
+++ 1063-best-sightseeing-pair/best-sightseeing-pair.cpp
@@ -1,17 +1,15 @@
+int IO = []{
+    ios::sync_with_stdio(false); cin.tie(nullptr); return 0;
+}();
 class Solution {
 public:
     int maxScoreSightseeingPair(vector<int>& values) {
-        int maxSoFar = values[0]; // Initialize with the first element (values[0] + 0)
+        int maxSoFar = values[0]; 
         int ans = INT_MIN;
-
         for (int i = 1; i < values.size(); ++i) {
-            // Calculate the current max sightseeing score
             ans = max(ans, maxSoFar + values[i] - i);
-
-            // Update maxSoFar for the next iteration
             maxSoFar = max(maxSoFar, values[i] + i);
         }
-
         return ans;
     }
 };
