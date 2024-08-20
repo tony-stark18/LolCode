a25d9af6c2351214bfd4c56841d86490e1be582b
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2024-08-20 11:55:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Anagram of String/anagram-of-string.cpp
+++ Difficulty: Basic/Anagram of String/anagram-of-string.cpp
@@ -27,27 +27,19 @@ int main()
 
 // function to calculate minimum numbers of characters
 // to be removed to make two strings anagram
-int remAnagram(string str1, string str2)
+int remAnagram(string s, string t)
 {
-    sort(str1.begin(),str1.end());
-    sort(str2.begin(),str2.end());
-    int ptr1 = 0;
-    int ptr2 = 0;
-    int count = 0;
-    while(ptr1<str1.length() && ptr2<str2.length()){
-        if(str1[ptr1]==str2[ptr2]){
-            ptr1++;
-            ptr2++;
-        }
-        else if(str1[ptr1]<str2[ptr2]){
-            count++;
-            ptr1++;
-        }
-        else{
-            count++;
-            ptr2++;
-        }
+    vector<int> arr1(26,0);
+    // vector<int> arr2(26,0);
+    for(int i=0;i<s.length();i++){
+        arr1[s[i]-'a']++;
     }
-    count+=str1.length()-ptr1+str2.length()-ptr2;
+    for(int i=0;i<t.length();i++){
+        arr1[t[i]-'a']--;
+    }
+    int count =0;
+    for(int i=0;i<26;i++){
+        count+=abs(arr1[i]);
+    }
     return count;
 }
