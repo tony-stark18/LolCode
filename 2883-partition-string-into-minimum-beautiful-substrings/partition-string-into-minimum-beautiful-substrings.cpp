8f386ab053535c4f49d092a156c088b4850813cc
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2024-09-25 16:15:27 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (82.29%) - LeetSync

--- 2883-partition-string-into-minimum-beautiful-substrings/partition-string-into-minimum-beautiful-substrings.cpp
+++ 2883-partition-string-into-minimum-beautiful-substrings/partition-string-into-minimum-beautiful-substrings.cpp
@@ -8,7 +8,7 @@ public:
             if (binaryString[i] == '1') {
                 n += base;
             }
-            base *= 2; // Move to the next power of 2
+            base *= 2;
         }
 
         while (n % 5 == 0) {
@@ -17,20 +17,23 @@ public:
 
         return n == 1;
     }
-    int f(int i,int n,string &s){
+    int f(int i,int n,string &s,vector<int> &dp){
         if(i==n) return 0;
+        if(dp[i]!=-1) return dp[i];
         int minAns = 1e9;
-        if(s[i]=='0') return 1e9;
+        if(s[i]=='0') return dp[i] = 1e9;
         for(int j=i;j<n;j++){
             if(s[j]!='0' && h(s,i,j)){
-                int ans = 1 + f(j+1,n,s);
+                int ans = 1 + f(j+1,n,s,dp);
                 minAns = min(ans,minAns);
             }
         }
-        return minAns;
+        return dp[i] = minAns;
     }
     int minimumBeautifulSubstrings(string s) {
-        int ans = f(0,s.length(),s);
+        int n = s.length();
+        vector<int> dp(n,-1);
+        int ans = f(0,n,s,dp);
         return ans>=1e9?-1:ans;
     }
 };
