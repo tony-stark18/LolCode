85bdd9b7d2380e6fa56d9ca6d5888daeedf48d12
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2024-09-25 15:12:21 +0530

    Time: 7 ms (81.41%) | Memory: 11.3 MB (45.88%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
@@ -1,23 +1,36 @@
 class Solution {
 public:
-    int f(int i, int k, int n, vector<int>& arr,vector<int> &dp) {
+    int f(int i, int k, int n, vector<int>& arr, vector<int>& dp) {
         if (i == n)
             return 0;
-        if(dp[i]!=-1) return dp[i];
+        if (dp[i] != -1)
+            return dp[i];
         int maxV = INT_MIN;
         int maxPoints = INT_MIN;
         for (int j = i; j < i + k; j++) {
             if (j < n) {
                 maxV = max(maxV, arr[j]);
-                int points = maxV * (j - i + 1) + f(j + 1, k, n, arr,dp);
-                maxPoints = max(maxPoints,points);
+                int points = maxV * (j - i + 1) + f(j + 1, k, n, arr, dp);
+                maxPoints = max(maxPoints, points);
             }
         }
         return dp[i] = maxPoints;
     }
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
         int n = arr.size();
-        vector<int> dp(n,-1);
-        return f(0, k, n , arr,dp);
+        vector<int> dp(n + 1, 0);
+        for (int i = n - 1; i >= 0; i--) {
+            int maxV = INT_MIN;
+            int maxPoints = INT_MIN;
+            for (int j = i; j < i + k; j++) {
+                if (j < n) {
+                    maxV = max(maxV, arr[j]);
+                    int points = maxV * (j - i + 1) + dp[j + 1];
+                    maxPoints = max(maxPoints, points);
+                }
+            }
+            dp[i] = maxPoints;
+        }
+        return dp[0];
     }
 };
