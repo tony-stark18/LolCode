684d7b157b5454ee3571b128b86f936245734bed
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2024-09-25 14:32:56 +0530

    Time: 1767 ms (11.96%) | Memory: 8 MB (89.81%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -1,17 +1,20 @@
-int IO = []{
-    ios::sync_with_stdio(false); cin.tie(nullptr); return 0;
+int IO = [] {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    return 0;
 }();
 class Solution {
 public:
-    bool isPalindrome(string &s, int start, int end) {
+    bool isPalindrome(string& s, int start, int end) {
         while (start <= end) {
             if (s[start++] != s[end--])
                 return false;
         }
         return true;
     }
-    int f(int i,int n, string &s, vector<int>& dp) {
-        if (i == n) return 0;
+    int f(int i, int n, string& s, vector<int>& dp) {
+        if (i == n)
+            return 0;
         if (dp[i] != -1)
             return dp[i];
         int min_cuts = 1e9;
@@ -23,23 +26,27 @@ public:
         }
         return dp[i] = min_cuts;
     }
-    int minCut(string s) {
-        int n = s.length();
-        // if (isPalindrome(s, 0, n - 1))
-        //     return 0;
-        vector<int> dp(n,-1);
-        // for (int i = n - 1; i >= 0; i--) {
-        //     for (int j = i; j <= n - 1; j++) {
-        //         int min_cuts = 1e9;
-        //         for (int k = i; k <= j; k++) {
-        //             if (isPalindrome(s, i, k)) {
-        //                 int cuts = 1 + dp[k + 1][j];
-        //                 min_cuts = min(cuts, min_cuts);
-        //             }
-        //         }
-        //         dp[i][j] = min_cuts;
-        //     }
-        // }
-        return f(0,n,s,dp)-1;
+    int minCut(string str) {
+        int n = str.size();
+        // Create a DP array to store the minimum number of partitions.
+        vector<int> dp(n + 1, 0);
+        dp[n] = 0; // Initialize the last element to 0.
+
+        // Loop through the string in reverse order.
+        for (int i = n - 1; i >= 0; i--) {
+            int minCost = INT_MAX;
+            // Consider all possible substrings starting from the current index.
+            for (int j = i; j < n; j++) {
+                if (isPalindrome(str,i,j)) {
+                    // If the substring is a palindrome, calculate the cost and
+                    // minimize it.
+                    int cost = 1 + dp[j + 1];
+                    minCost = min(minCost, cost);
+                }
+            }
+            dp[i] = minCost;
+        }
+        // Subtract 1 as it counts partitions, not cuts.
+        return dp[0] - 1;
     }
 };
