7f6c1924f7ab6cc5a7ef67cb60e3aeff076f8b4f
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2024-09-30 21:45:43 +0530

    Time: 15 ms (99.30%) | Memory: 26 MB (66.84%) - LeetSync

--- 1497-design-a-stack-with-increment-operation/design-a-stack-with-increment-operation.cpp
+++ 1497-design-a-stack-with-increment-operation/design-a-stack-with-increment-operation.cpp
@@ -1,45 +1,38 @@
 class CustomStack {
 public:
+    vector<int> st;
+    vector<int> inc;
     int maxSize;
-    stack<int> st;
-    int size;
     
     CustomStack(int maxSize) {
         this->maxSize = maxSize;
-        size = 0; 
+        st.resize(0);
+        inc.resize(maxSize, 0);
     }
     
     void push(int x) {
-        if (size >= maxSize) return; // Fix: Prevent exceeding maxSize
-        st.push(x);
-        size++;
+        if (st.size() < maxSize) {
+            st.push_back(x);
+        }
     }
     
     int pop() {
-        if (st.empty()) return -1;
-        int val = st.top();
-        st.pop();
-        size--;  
-        return val;  
+        int idx = st.size() - 1;
+        if (idx < 0) return -1;
+        
+        if (idx > 0) {
+            inc[idx - 1] += inc[idx]; 
+        }
+        int res = st.back() + inc[idx];
+        inc[idx] = 0; 
+        st.pop_back();
+        return res;
     }
     
     void increment(int k, int val) {
-        stack<int> temp;
-        while (!st.empty()) {
-            temp.push(st.top());
-            st.pop();
-        }
-        
-        int count = 0; 
-        while (!temp.empty() && count < k) {
-            st.push(temp.top() + val);
-            temp.pop();
-            count++;
-        }
-        
-        while (!temp.empty()) {
-            st.push(temp.top());
-            temp.pop();
+        int idx = min(k, (int)st.size()) - 1; 
+        if (idx >= 0) {
+            inc[idx] += val; 
         }
     }
 };
@@ -48,4 +41,6 @@ public:
  * Your CustomStack object will be instantiated and called as such:
  * CustomStack* obj = new CustomStack(maxSize);
  * obj->push(x);
- **/
+ * int param_2 = obj->pop();
+ * obj->increment(k,val);
+ */
