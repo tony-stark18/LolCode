9ba687e89ead73adf55606938ff3000bf35f89a1
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2024-10-04 19:20:49 +0530

    Time: 27 ms (100.00%) | Memory: 55.4 MB (100.00%) - LeetSync

--- 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.cpp
+++ 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.cpp
@@ -1,21 +1,38 @@
+int IO = []{
+    ios::sync_with_stdio(false); cin.tie(nullptr); return 0;
+}();
 class Solution {
 public:
-    long long dividePlayers(vector<int>& skill) {
-        sort(skill.begin(),skill.end());
-        int i = 0;
-        int j = skill.size()-1;
-        long long sum = skill[i]+skill[j];
-        long long chemistry = skill[i]*skill[j];
-        i++;
-        j--;
-        while(i<j){
-            if(skill[i]+skill[j]!=sum){
-                return -1;
-            }
-            chemistry += (skill[i]*skill[j]);
-            i++;
-            j--; 
+    void sortS(vector<int>& skill) {
+        int maxEl = *max_element(skill.begin(), skill.end());;
+        vector<int> freq(maxEl + 1, 0);
+        for (int i : skill) {
+            freq[i]++;
         }
+        int k = 0;
+        for (int i = 0; i <= maxEl; i++) {
+            while (freq[i]--) {
+                skill[k++] = i;
+            }
+        }
+    }
+    long long dividePlayers(vector<int>& skill) {
+        sortS(skill);
+        int i = 0;
+        int j = skill.size() - 1;
+
+        long long sum = skill[i] + skill[j];
+        long long chemistry = 0;
+
+        while (i < j) {
+            if (skill[i] + skill[j] != sum) {
+                return -1; 
+            }
+            chemistry += (skill[i] * skill[j]);
+            i++;
+            j--;
+        }
+
         return chemistry;
     }
 };
