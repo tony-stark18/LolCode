187c7ffa3884648df4ceefe995469fd8e1d9e6f7
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2024-10-10 20:00:52 +0530

    Time: 71 ms (15.79%) | Memory: 47.1 MB (23.94%) - LeetSync

--- 1002-maximum-width-ramp/maximum-width-ramp.cpp
+++ 1002-maximum-width-ramp/maximum-width-ramp.cpp
@@ -4,27 +4,18 @@ public:
         stack<int> st;
         int n = nums.size();
         int max_ramp = 0;
+        for (int i = 0; i < n; i++) {
+            if (st.empty() || nums[st.top()] > nums[i]) {
+                st.push(i);
+            }
+        }
         for (int i = n - 1; i >= 0; i--) {
-            if (st.empty()) {
-                st.push(i);
-                continue;
+            int ramp = 0;
+            while (!st.empty() && nums[st.top()] <= nums[i]) {
+                ramp = i - st.top();
+                st.pop();
             }
-            if (nums[i] > nums[st.top()]) {
-                st.push(i);
-            } else {
-                stack<int> temp;
-                int ramp = 0;
-                while(!st.empty() && nums[st.top()]>=nums[i]){
-                    ramp = st.top()-i;
-                    temp.push(st.top());
-                    st.pop();
-                }
-                while(!temp.empty()){
-                    st.push(temp.top());
-                    temp.pop();
-                }
-                max_ramp = max(ramp,max_ramp);
-            }
+            max_ramp = max(ramp, max_ramp);
         }
         return max_ramp;
     }
