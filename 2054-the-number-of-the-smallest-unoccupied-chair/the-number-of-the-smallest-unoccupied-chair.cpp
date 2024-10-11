070e0270ac98d08be64f1cd4e207197488862318
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2024-10-11 17:04:32 +0530

    Time: 155 ms (49.83%) | Memory: 63.4 MB (52.92%) - LeetSync

--- 2054-the-number-of-the-smallest-unoccupied-chair/the-number-of-the-smallest-unoccupied-chair.cpp
+++ 2054-the-number-of-the-smallest-unoccupied-chair/the-number-of-the-smallest-unoccupied-chair.cpp
@@ -2,8 +2,10 @@ class Solution {
 public:
     int smallestChair(vector<vector<int>>& times, int targetFriend) {
         int n = times.size();
-        vector<int> chairs(n, -1); 
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq; 
+        
+        priority_queue<int, vector<int>, greater<int>> chairs;
+     
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
         
         for (int i = 0; i < n; i++) {
             times[i].push_back(i); 
@@ -12,26 +14,27 @@ public:
         sort(times.begin(), times.end());
 
         for (int i = 0; i < n; i++) {
+            chairs.push(i);
+        }
+
+        for (int i = 0; i < n; i++) {
             int at = times[i][0];        
-            int lt = times[i][1];       
+            int lt = times[i][1];        
             int friendIdx = times[i][2]; 
 
+            
             while (!pq.empty() && pq.top().first <= at) {
-                chairs[pq.top().second] = -1; 
+                chairs.push(pq.top().second); 
                 pq.pop();
             }
 
-            int chairIndex = -1;
-            for (int j = 0; j < n; j++) {
-                if (chairs[j] == -1) {
-                    chairs[j] = friendIdx;
-                    chairIndex = j;
-                    if (friendIdx == targetFriend) {
-                        return j;
-                    }
-                    break;
-                }
+            int chairIndex = chairs.top();
+            chairs.pop();
+
+            if (friendIdx == targetFriend) {
+                return chairIndex;
             }
+
             pq.push({lt, chairIndex});
         }
 
