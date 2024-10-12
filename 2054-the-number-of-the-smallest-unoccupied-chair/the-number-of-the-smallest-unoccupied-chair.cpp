0703f42cdf30ca19072dfedf8b656e6b4b1d1812
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2024-10-12 13:02:42 +0530

    Time: 154 ms (50.85%) | Memory: 63.5 MB (53.24%) - LeetSync

--- 2054-the-number-of-the-smallest-unoccupied-chair/the-number-of-the-smallest-unoccupied-chair.cpp
+++ 2054-the-number-of-the-smallest-unoccupied-chair/the-number-of-the-smallest-unoccupied-chair.cpp
@@ -1,43 +1,29 @@
 class Solution {
 public:
     int smallestChair(vector<vector<int>>& times, int targetFriend) {
+        priority_queue<int,vector<int>,greater<int>> chairs;
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
         int n = times.size();
-        
-        priority_queue<int, vector<int>, greater<int>> chairs;
-     
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-        
-        for (int i = 0; i < n; i++) {
-            times[i].push_back(i); 
-        }
-
-        sort(times.begin(), times.end());
-
-        for (int i = 0; i < n; i++) {
+        for(int i=0;i<n;i++){
+            times[i].push_back(i);
             chairs.push(i);
         }
+        sort(times.begin(),times.end());
+        for(int i=0;i<n;i++){
+            int at = times[i][0];
+            int lt = times[i][1];
+            int oi = times[i][2];
 
-        for (int i = 0; i < n; i++) {
-            int at = times[i][0];        
-            int lt = times[i][1];        
-            int friendIdx = times[i][2]; 
-
-            
-            while (!pq.empty() && pq.top().first <= at) {
-                chairs.push(pq.top().second); 
+            while(!pq.empty() && pq.top().first<=at){
+                chairs.push(pq.top().second);
                 pq.pop();
             }
 
-            int chairIndex = chairs.top();
+            int avail_chair = chairs.top();
+            if(oi==targetFriend) return avail_chair;
+            pq.push({lt,avail_chair});
             chairs.pop();
-
-            if (friendIdx == targetFriend) {
-                return chairIndex;
-            }
-
-            pq.push({lt, chairIndex});
         }
-
         return -1;
     }
 };
