fa8380dfe27ee35a5b509b9051d4502f76fea444
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2024-10-17 14:36:13 +0530

    Time: 3 ms (38.18%) | Memory: 7.5 MB (70.34%) - LeetSync

--- 670-maximum-swap/maximum-swap.cpp
+++ 670-maximum-swap/maximum-swap.cpp
@@ -3,22 +3,24 @@ public:
     int maximumSwap(int num) {
         string s = to_string(num);
         int n = s.length();
-        vector<int> temp(n,-1);
-        for(int i=0;i<n;i++){
-            char maxI = i;
-            for(int j=i+1;j<n;j++){
-                if(s[j]>=s[maxI] && s[i]!=s[j]){
-                    maxI = j;
-                }   
-            }
-            temp[i] = maxI;
-        }
-        for(int i=0;i<n;i++){
-            if(temp[i]!=i){
-                swap(s[i],s[temp[i]]);
-                return stoi(s);
+        vector<int> maxIdx(n);
+        
+        maxIdx[n-1] = n-1; 
+        for (int i = n - 2; i >= 0; --i) {
+            if (s[i] > s[maxIdx[i + 1]]) {
+                maxIdx[i] = i;
+            } else {
+                maxIdx[i] = maxIdx[i + 1];
             }
         }
+        
+        for (int i = 0; i < n; ++i) {
+            if (s[i] != s[maxIdx[i]]) {
+                swap(s[i], s[maxIdx[i]]);
+                break;
+            }
+        }
+        
         return stoi(s);
     }
 };
