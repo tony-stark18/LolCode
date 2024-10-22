40fdd9fdbc763e41b5b3597d0b63a2136e83f45b
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2024-10-22 10:56:49 +0530

    Time: 810 ms (9.67%) | Memory: 170.1 MB (5.08%) - LeetSync

--- 2646-kth-largest-sum-in-a-binary-tree/kth-largest-sum-in-a-binary-tree.cpp
+++ 2646-kth-largest-sum-in-a-binary-tree/kth-largest-sum-in-a-binary-tree.cpp
@@ -6,35 +6,41 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    int get_depth(TreeNode* root){
-        if(root==nullptr){
-            return 0;
-        }
-        int ld = get_depth(root->left);            
-        int rd = get_depth(root->right);  
-        return max(ld,rd)+1;
-    }
-    void soln(TreeNode* root,vector<long long> &sums,int d){
-        if(root==nullptr){
+    // int get_depth(TreeNode* root){
+    //     if(root==nullptr){
+    //         return 0;
+    //     }
+    //     int ld = get_depth(root->left);
+    //     int rd = get_depth(root->right);
+    //     return max(ld,rd)+1;
+    // }
+    void soln(TreeNode* root, vector<long long>& sums, int d) {
+        if (root == nullptr) {
             return;
         }
-        sums[d]+=root->val;
-        soln(root->left,sums,d+1);
-        soln(root->right,sums,d+1);
+        if (sums.size() > d) {
+            sums[d] += root->val;
+        }
+        else {
+            sums.push_back(root->val);
+        }
+        soln(root->left, sums, d + 1);
+        soln(root->right, sums, d + 1);
     }
     long long kthLargestLevelSum(TreeNode* root, int k) {
-        int depth = get_depth(root);
-        if(k>depth) return -1;
-        vector<long long> sums(depth);
-        soln(root,sums,0);
-        sort(sums.begin(),sums.end(),[](long long a,long long b){
-            return a>b;
-        });
-        return sums[k-1];
+        // int depth = get_depth(root);
+        // if(k>depth) return -1;
+        vector<long long> sums;
+        soln(root, sums, 0);
+        sort(sums.begin(), sums.end(),
+             [](long long a, long long b) { return a > b; });
+        if(sums.size()<k) return -1;
+        return sums[k - 1];
     }
 };
