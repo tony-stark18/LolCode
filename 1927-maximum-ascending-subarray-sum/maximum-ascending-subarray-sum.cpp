ca1fafa13c31914753132a7235756997904dbf0b
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2025-02-04 09:36:25 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (77.09%) - LeetSync

---
+++ 1927-maximum-ascending-subarray-sum/maximum-ascending-subarray-sum.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxAscendingSum(vector<int>& nums) {
+        int sum = nums[0];
+        int ans = sum;
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]>nums[i-1]) sum+=nums[i];
+            else sum=nums[i];
+            ans = max(ans,sum);
+        }
+        return ans;
+    }
+};
