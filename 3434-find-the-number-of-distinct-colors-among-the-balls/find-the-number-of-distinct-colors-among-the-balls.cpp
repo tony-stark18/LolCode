0f774bc477e56b6d54a3a7978263164f8661ef05
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2025-02-07 10:08:03 +0530

    Time: 97 ms (97.43%) | Memory: 157.5 MB (56.59%) - LeetSync

--- 3434-find-the-number-of-distinct-colors-among-the-balls/find-the-number-of-distinct-colors-among-the-balls.cpp
+++ 3434-find-the-number-of-distinct-colors-among-the-balls/find-the-number-of-distinct-colors-among-the-balls.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     vector<int> queryResults(int limit, vector<vector<int>>& queries) {
-        unordered_map<int, int> balls;  // Maps index to color
-        unordered_map<int, int> colors; // Maps color to count
+        unordered_map<int, int> balls; 
+        unordered_map<int, int> colors;
         vector<int> ans;
         
         for (const auto &q : queries) {
             int index = q[0], color = q[1];
             
-            if (balls.count(index)) { // If the index already has a ball
+            if (balls.count(index)) {
                 int prevColor = balls[index];
-                if (--colors[prevColor] == 0) { // Decrement color count and remove if zero
+                if (--colors[prevColor] == 0) {
                     colors.erase(prevColor);
                 }
             }
             
-            balls[index] = color; // Assign new color
-            colors[color]++; // Increment new color count
+            balls[index] = color;
+            colors[color]++;
             
             ans.push_back(min((int)colors.size(), (int)balls.size()));
         }
