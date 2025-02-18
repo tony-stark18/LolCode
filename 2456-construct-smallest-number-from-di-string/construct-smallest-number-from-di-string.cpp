7723cf3f7ed91beb00b58e2c56bd3922a1dadc92
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2025-02-18 22:51:48 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (49.49%) - LeetSync

--- 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.cpp
+++ 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.cpp
@@ -1,48 +1,19 @@
 class Solution {
 public:
-    int visited[10];
-    bool flag = true;
-    vector<string> res;
-    void soln(int ind,int num,string pattern,string ans){
-        if(ind==pattern.length()){
-            flag= false;
-            res.push_back(ans);
-            return;
-        }
-        char c = pattern[ind];
-        if(c=='I'){
-            for(int i=1;i<=9;i++){
-                if(!visited[i] && flag && i>num){
-                    visited[i]=1;
-                    char t = '0'+i;
-                    soln(ind+1,i,pattern,ans+t);
-                    visited[i]=0;
-                }
-            }
-        }
-        else{
-            for(int i=1;i<=9;i++){
-                if(!visited[i] && flag && i<num){
-                    visited[i]=1;
-                    char t = '0'+i;
-                    soln(ind+1,i,pattern,ans+t);
-                    visited[i]=0;
-                }
-            }
-        }
-    }
     string smallestNumber(string pattern) {
-        int n= pattern.length();
-        for(int i=1;i<=9;i++){
-            char c = '0'+i;
-            string s = "";
-            s+=c;
-            visited[i]=1;
-            soln(0,i,pattern,s);
-            visited[i]=0;
+        int n = pattern.size();
+        string result;
+        stack<int> st;
+
+        for (int i = 0; i <= n; i++) {
+            st.push(i + 1);
+            if (i == n || pattern[i] == 'I') {
+                while (!st.empty()) {
+                    result += ('0' + st.top());
+                    st.pop();
+                }
+            }
         }
-        sort(res.begin(),res.end());
-        cout<<res[0];
-        return res[0];
+        return result;
     }
 };
