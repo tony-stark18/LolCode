62b57b766296fb3c87f117adbf95bc96092647ca
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2025-02-18 23:06:51 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (49.49%) - LeetSync

--- 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.cpp
+++ 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.cpp
@@ -5,11 +5,11 @@ public:
         string result;
         stack<int> st;
 
-        for (int i = 0; i <= n; i++) {
-            st.push(i + 1);
-            if (i == n || pattern[i] == 'I') {
-                while (!st.empty()) {
-                    result += ('0' + st.top());
+        for(int i=0;i<=n;i++){
+            st.push(i+1);
+            if(i==n || pattern[i]=='I'){
+                while(!st.empty()){
+                    result+=('0'+st.top());
                     st.pop();
                 }
             }
