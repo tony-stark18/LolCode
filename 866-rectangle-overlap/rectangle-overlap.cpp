2af77fedff736fbf18e494261605c4a6a5c5aaaa
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2025-02-22 00:02:25 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (35.38%) - LeetSync

--- 866-rectangle-overlap/rectangle-overlap.cpp
+++ 866-rectangle-overlap/rectangle-overlap.cpp
@@ -1,40 +1,7 @@
 class Solution {
 public:
     bool isRectangleOverlap(vector<int>& rect1, vector<int>& rect2) {
-        int x1 = rect1[0];
-        int y1 = rect1[1];
-        int x2 = rect1[2];
-        int y2 = rect1[3];
-        int xx1 = rect2[0];
-        int yy1 = rect2[1];
-        int xx2 = rect2[2];
-        int yy2 = rect2[3];
-        if(xx1>x1){
-            if(yy1>y1){
-                return xx1<x2 && yy1<y2;
-            }
-            if(y1>yy1){
-                return xx1<x2 && y1<yy2;
-            }
-            return xx1<x2;
-        }
-        if(x1>xx1){
-            if(yy1>y1){
-                return xx2>x1 && yy1<y2;
-            }
-            if(y1>yy1){
-                return xx2>x1 && y1<yy2;
-            }
-            return xx2>x1;
-        }
-        else{
-            if(yy1>y1){
-                return yy1<y2;
-            }
-            if(y1>yy1){
-                return y1<yy2;
-            }
-        }
-        return false;
+        return max(rect1[0], rect2[0]) < min(rect1[2], rect2[2]) &&
+               max(rect1[1], rect2[1]) < min(rect1[3], rect2[3]);
     }
 };
