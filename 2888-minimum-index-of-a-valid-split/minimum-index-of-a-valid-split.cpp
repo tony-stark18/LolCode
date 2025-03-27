3cc5c80494795a09848c15c5fb812fcbfa783678
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2025-03-27 11:42:56 +0530

    Time: 3 ms (80.74%) | Memory: 89.9 MB (85.66%) - LeetSync

--- 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.cpp
+++ 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.cpp
@@ -1,42 +1,29 @@
 class Solution {
 public:
-    int getMajority(vector<int> &nums,vector<int> &fromLeft,vector<int> &fromRight){
+    int getMajority(vector<int> &nums){
         int n = nums.size();
-        int countL=0,countR = 0;
-        int meL = -1;
-        int meR = -1;
+        int count = 0;
+        int me = -1;
         for(int i=0;i<n;i++){
-            if(countL==0){
-                countL=1;
-                meL = nums[i];
+            if(count==0){
+                count=1;
+                me = nums[i];
             } else{
-                if(nums[i]==meL) countL++;
-                else countL--;
+                if(nums[i]==me) count++;
+                else count--;
             }
-            fromLeft[i]=(countL>0)?meL:-1;
-            if(countR==0){
-                countR = 1;
-                meR = nums[n-i-1];
-            } else{
-                if(nums[n-1-i]==meR) countR++;
-                else countR--;
-            }
-            fromRight[n-i-1]=(countR>0)?meR:-1;
         }
-        return meL;
+        return me;
     }
     int minimumIndex(vector<int>& nums) {
         int n = nums.size();
-        vector<int> fromLeft(n),fromRight(n);
-        // unordered_map<int,int> map,map2;
-        int me = getMajority(nums,fromLeft,fromRight);
+
+        int me = getMajority(nums);
         int count = 0,count2 = 0;
         for(int i=0;i<n;i++) count+=nums[i]==me;
-        // cout<<me<<endl;
         for(int i=0;i<n-1;i++){
             count2+=nums[i]==me;
-            if(fromLeft[i]==me && count2*2>i+1 && fromRight[i+1]==me && (count-count2)*2>n-i-1) return i;
-            // cout<<i<<" "<<fromLeft[i]<<" "<<fromRight[i]<<", ";
+            if(count2*2>i+1 && (count-count2)*2>n-i-1) return i;
         }
         return -1;
     }
