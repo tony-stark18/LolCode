4ceb776ae345bf7570b261a03e0d0faae51e487e
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2025-08-20 22:34:20 +0530

    Time: 320 ms (15.76%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -4,3 +4,5 @@ w2.id as Id from Weather w1
 join Weather w2 
 on w2.recorddate - w1.recorddate = 1
 where w2.temperature > w1.temperature;
+
+-- select * from Weather w1 inner join Weather w2 on w1.temperature+w2.temperature > 50;
