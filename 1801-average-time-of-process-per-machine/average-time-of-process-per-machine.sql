4ec9c0cc9ce003ace2894b50ed5f67f9d6faa030
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2025-12-17 11:15:18 +0530

    Time: 166 ms (70.55%) | Memory: 0B (100.00%) - LeetSync

--- 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -1,5 +1,6 @@
 -- Write your PostgreSQL query statement below
-select a.machine_id,ROUND((sum(a.timestamp-b.timestamp)::numeric/count(a.process_id)::numeric),3) as processing_time  
+select a.machine_id,
+ROUND((sum(a.timestamp-b.timestamp)::numeric/count(a.process_id)::numeric),3) as processing_time  
 from Activity a join Activity b
 on a.machine_id = b.machine_id
 and a.process_id = b.process_id
