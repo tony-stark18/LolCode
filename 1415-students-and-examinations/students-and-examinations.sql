6cfa05708fdb00f6ad92cf2cf93ec88ef2eafe10
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2025-12-18 01:33:05 +0530

    Time: 396 ms (43.57%) | Memory: 0B (100.00%) - LeetSync

--- 1415-students-and-examinations/students-and-examinations.sql
+++ 1415-students-and-examinations/students-and-examinations.sql
@@ -1,14 +1,11 @@
 -- Write your PostgreSQL query statement below
-with exams_per_student as(
-    select e.student_id as student_id,e.subject_name as subject_name,count(*) as attended_exams from Examinations e
-    group by 1,2
-),
-students_with_exams as(
-    select * from students s cross join subjects
-    order by s.student_id,subject_name asc
-)
-select swe.*,COALESCE(eps.attended_exams, 0) as attended_exams
-from students_with_exams as swe
-left join exams_per_student as eps
-on eps.student_id = swe.student_id
-and eps.subject_name = swe.subject_name
+select  st.student_id as student_id,
+        st.student_name as student_name,
+        sb.subject_name as subject_name,
+        count(ex.subject_name) as attended_exams
+from students st cross join subjects sb
+left join examinations ex
+on ex.student_id = st.student_id
+and ex.subject_name = sb.subject_name
+group by 1,2,3
+order by 1,3
