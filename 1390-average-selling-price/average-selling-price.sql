bc083f91aaebefad3b4770cdb302ed9af3b95b5f
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2025-12-18 23:41:31 +0530

    Time: 217 ms (33.83%) | Memory: 0B (100.00%) - LeetSync

--- 1390-average-selling-price/average-selling-price.sql
+++ 1390-average-selling-price/average-selling-price.sql
@@ -1,5 +1,5 @@
 -- Write your PostgreSQL query statement below
-SELECT P.product_id,COALESCE(ROUND((SUM(P.price * U.units)::float/SUM(U.units)::float)::numeric,2),0) as average_price FROM Prices P
+SELECT P.product_id,COALESCE(ROUND((SUM(P.price * U.units)/SUM(U.units*1.00)),2),0) as average_price FROM Prices P
 LEFT JOIN UnitsSold U
 ON P.product_id = U.product_id
 AND U.purchase_date BETWEEN P.start_date AND P.end_date
