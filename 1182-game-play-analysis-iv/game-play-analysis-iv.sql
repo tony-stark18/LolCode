2a1cb0fdc5b0bb157ff809a7337139d37ab0da2f
Diptiranjan Sahoo <114793894+tony-stark18@users.noreply.github.com>
2025-12-22 00:00:36 +0530

    Time: 513 ms (51.08%) | Memory: 0B (100.00%) - LeetSync

--- 1182-game-play-analysis-iv/game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -1,12 +1,16 @@
--- Write your PostgreSQL query statement below
-WITH activity_with_initial_login AS(
-    SELECT a.player_id,MIN(a.event_date) as event_date
-    FROM Activity a
-    GROUP BY 1
-) 
-SELECT 
-    ROUND(COUNT(DISTINCT a.player_id)*1.00/(SELECT COUNT(DISTINCT c.player_id) FROM Activity c),2) as fraction
-    FROM Activity a
-    JOIN activity_with_initial_login b
-    ON a.event_date - b.event_date = 1 
-    and a.player_id = b.player_id
+SELECT
+    ROUND(
+        COUNT(*)*1.00
+        / (SELECT COUNT(DISTINCT player_id) FROM Activity),
+        2
+    ) AS fraction
+FROM (
+    SELECT player_id
+    FROM Activity
+    GROUP BY player_id
+    HAVING MIN(event_date) + 1 IN (
+        SELECT event_date
+        FROM Activity a2
+        WHERE a2.player_id = Activity.player_id
+    )
+) t;
